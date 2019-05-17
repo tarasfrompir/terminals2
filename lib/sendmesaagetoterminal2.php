e629c3ad5c564dc81bd534c4ff55a59641fe84b4
tarasfrompir <tarasfrompir@inbox.ru>
2019-05-17 09:37:57 +0300

    Update sendmesaagetoterminal2.php

--- lib/sendmesaagetoterminal2.php
+++ lib/sendmesaagetoterminal2.php
@@ -1,14 +1,13 @@
 <?php
-function send_message_to_terminal ($terminal, $message, $event, $member, $level, $filename, $linkfile, $lang, $langfull ) {
+function send_message_to_terminal($terminal, $message, $event, $member, $level, $filename, $linkfile, $lang, $langfull ) {
     if (!$terminal) {
         return 0;
     }
 	
     $url = BASE_URL . ROOTHTML . 'ajax/app_player.html?';
-    $url .= "&command=" . ($safe_say ? 'safe_say' : 'say');
     $url .= "&command=say";
     $url .= "&play_terminal=" . $terminal;
-    $url .= "&param=" . urlencode($terminal.','.$message.','.$event.','.$member.','.$level.','.$linkfile.','.$lang.','.$langfull);
+    $url .= "&param=" . urlencode($terminal.','.$message.','.$event.','.$member.','.$level.','.$filename.','.$linkfile.','.$lang.','.$langfull);
     getURLBackground($url);
     return 1;
 }
