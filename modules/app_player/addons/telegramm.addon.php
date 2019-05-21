4e3ad9c00c05a608c17b8997f067b7cde991c4fb
tarasfrompir <tarasfrompir@inbox.ru>
2019-05-21 19:13:40 +0300

    Update telegramm.addon.php

--- modules/app_player/addons/telegramm.addon.php
+++ modules/app_player/addons/telegramm.addon.php
@@ -23,7 +23,7 @@ class telegramm extends app_player_addon {
         if(file_exists(DIR_MODULES.'telegram/telegram.class.php')) {
 	    $out = explode(',', $params);
 	    $message = $out[1];
-            $users = SQLSelect("SELECT * FROM tlg_user WHERE HISTORY=1;");
+            $users = SQLSelect("SELECT * FROM tlg_user ");
 	    $c_users = count($users);
 
             if($message AND $c_users) {
