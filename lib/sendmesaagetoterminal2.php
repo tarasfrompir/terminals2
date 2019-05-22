6e0ca685b788b757e0b61ac792cc24f20d31455b
tarasfrompir <tarasfrompir@inbox.ru>
2019-05-22 20:58:18 +0300

    Update sendmesaagetoterminal2.php

--- lib/sendmesaagetoterminal2.php
+++ lib/sendmesaagetoterminal2.php
@@ -11,3 +11,24 @@ function send_message_to_terminal($terminal, $message, $event, $member, $level, 
     getURLBackground($url);
     return 1;
 }
+
+/**
+ * Summary of playMedia
+ * @param mixed $path Path
+ * @param mixed $host Host (default 'localhost')
+ * @return int
+ */
+function saytts($terminal, $message, $event, $member, $level, $lang, $langfull) {
+    if (!$terminal) {
+        return 0;
+    }
+	
+    $url = BASE_URL . ROOTHTML . 'ajax/app_player.html?';
+    $url .= "&command=" . 'say_tts';
+    $url .= "&play_terminal=" . $terminal;
+    $url .= "&param=" . urlencode($terminal.','.$message.','.$event.','.$member.','.$level.','.$lang.','.$langfull);
+    //DebMes($url,'playmedia');
+    getURLBackground($url);
+    return 1;
+
+}
