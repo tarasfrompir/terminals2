d1e0743d5431e99149c9ad0ef5570591d5e8f242
tarasfrompir <tarasfrompir@inbox.ru>
2019-06-04 16:02:11 +0300

    Update majordroid.addon.php

--- modules/app_player/addons/majordroid.addon.php
+++ modules/app_player/addons/majordroid.addon.php
@@ -1,142 +1,147 @@
 <?php
 
 /*
-	Addon MajorDroid for app_player
+Addon MajorDroid for app_player
 */
 
-class majordroid extends app_player_addon {
-
-	// Constructor
-	function __construct($terminal) {
-		$this->title = 'MajorDroid';
-		$this->description = 'Описание: Терминал предназначен для устройств использующих MajorDroidAPI.';
-		
-		$this->terminal = $terminal;
-		$this->reset_properties();
-		
-		// Network
-		$this->terminal['PLAYER_PORT'] = (empty($this->terminal['PLAYER_PORT'])?7999:$this->terminal['PLAYER_PORT']);
-	}
-
-	// saytts
-	function sayttotext($message, $event, $lang, $langfull) {
-            $this->reset_properties();
-	    if(strlen($message)) {
-			$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-			if($socket === false) {
-				$this->success = FALSE;
-				$this->message = socket_strerror(socket_last_error());
-				$this->message = iconv('CP1251', 'UTF-8', $this->message);
-			} else {
-				$result = @socket_connect($socket, $this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
-				if($result === false) {
-					$this->success = FALSE;
-					$this->message = socket_strerror(socket_last_error($socket));
-					$this->message = iconv('CP1251', 'UTF-8', $this->message);
-				} else {
-					if ($event=='SAY' OR $event = 'SAYTO' OR $event = 'SAYREPLY') {
-						$packet = 'tts:'.$message;
-					} else {
-						$packet = 'ask:'.$message;
-					}
-					socket_write($socket, $packet, strlen($packet));
-					$this->success = TRUE;
-					$this->message = 'OK';
-				}
-				socket_close($socket);
-			}
-		} else {
-			$this->success = FALSE;
-			$this->message = 'Input is missing!';
-		}
-		return $this->success;
-	}
-	
-	// Play
-	function play($input) {
-		$this->reset_properties();
-		if(strlen($input)) {
-			$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-			if($socket === false) {
-				$this->success = FALSE;
-				$this->message = socket_strerror(socket_last_error());
-				$this->message = iconv('CP1251', 'UTF-8', $this->message);
-			} else {
-				$result = @socket_connect($socket, $this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
-				if($result === false) {
-					$this->success = FALSE;
-					$this->message = socket_strerror(socket_last_error($socket));
-					$this->message = iconv('CP1251', 'UTF-8', $this->message);
-				} else {
-					$packet = 'play:'.$input;
-					socket_write($socket, $packet, strlen($packet));
-					$this->success = TRUE;
-					$this->message = 'OK';
-				}
-				socket_close($socket);
-			}
-		} else {
-			$this->success = FALSE;
-			$this->message = 'Input is missing!';
-		}
-		return $this->success;
-	}
-	
-	// Pause
-	function pause() {
-		$this->reset_properties();
-		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-		if($socket === false) {
-			$this->success = FALSE;
-			$this->message = socket_strerror(socket_last_error());
-			$this->message = iconv('CP1251', 'UTF-8', $this->message);
-		} else {
-			$result = @socket_connect($socket, $this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
-			if($result === false) {
-				$this->success = FALSE;
-				$this->message = socket_strerror(socket_last_error($socket));
-				$this->message = iconv('CP1251', 'UTF-8', $this->message);
-			} else {
-				$packet = 'pause';
-				socket_write($socket, $packet, strlen($packet));
-				$this->success = TRUE;
-				$this->message = 'OK';
-			}
-			socket_close($socket);
-		}
-		return $this->success;
-	}
-
-	// Stop
-	function stop() {
-		return $this->pause();
-	}
-
-	// Default command
-	function command($command, $parameter) {
-		$this->reset_properties();
-		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-		if($socket === false) {
-			$this->success = FALSE;
-			$this->message = socket_strerror(socket_last_error());
-			$this->message = iconv('CP1251', 'UTF-8', $this->message);
-		} else {
-			$result = @socket_connect($socket, $this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
-			if($result === false) {
-				$this->success = FALSE;
-				$this->message = socket_strerror(socket_last_error($socket));
-				$this->message = iconv('CP1251', 'UTF-8', $this->message);
-			} else {
-				$packet = $command.(strlen($parameter)?':'.$parameter:'');
-				socket_write($socket, $packet, strlen($packet));
-				$this->success = TRUE;
-				$this->message = 'OK';
-			}
-			socket_close($socket);
-		}
-		return $this->success;
-	}
-	
+class majordroid extends app_player_addon
+{
+    
+    // Constructor
+    function __construct($terminal)
+    {
+        $this->title       = 'MajorDroid';
+        $this->description = 'Описание: Терминал предназначен для устройств использующих MajorDroidAPI.';
+        $this->terminal = $terminal;
+        $this->reset_properties();
+        // Network
+        $this->terminal['PLAYER_PORT'] = (empty($this->terminal['PLAYER_PORT']) ? 7999 : $this->terminal['PLAYER_PORT']);
+    }
+    
+    // saytts
+    function sayttotext($message, $event, $lang, $langfull)
+    {
+        $this->reset_properties();
+        if (strlen($message)) {
+            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            if ($socket === false) {
+                $this->success = FALSE;
+                $this->message = socket_strerror(socket_last_error());
+                $this->message = iconv('CP1251', 'UTF-8', $this->message);
+            } else {
+                $result = @socket_connect($socket, $this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
+                if ($result === false) {
+                    $this->success = FALSE;
+                    $this->message = socket_strerror(socket_last_error($socket));
+                    $this->message = iconv('CP1251', 'UTF-8', $this->message);
+                } else {
+                    if ($event == 'SAY' OR $event = 'SAYTO' OR $event = 'SAYREPLY') {
+                        $packet = 'tts:' . $message;
+                    } else {
+                        $packet = 'ask:' . $message;
+                    }
+                    socket_write($socket, $packet, strlen($packet));
+                    $this->success = TRUE;
+                    $this->message = 'OK';
+                }
+                socket_close($socket);
+            }
+        } else {
+            $this->success = FALSE;
+            $this->message = 'Input is missing!';
+        }
+        return $this->success;
+    }
+    
+    // Play
+    function play($input)
+    {
+        $this->reset_properties();
+        if (strlen($input)) {
+            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            if ($socket === false) {
+                $this->success = FALSE;
+                $this->message = socket_strerror(socket_last_error());
+                $this->message = iconv('CP1251', 'UTF-8', $this->message);
+            } else {
+                $result = @socket_connect($socket, $this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
+                if ($result === false) {
+                    $this->success = FALSE;
+                    $this->message = socket_strerror(socket_last_error($socket));
+                    $this->message = iconv('CP1251', 'UTF-8', $this->message);
+                } else {
+                    $packet = 'play:' . $input;
+                    socket_write($socket, $packet, strlen($packet));
+                    $this->success = TRUE;
+                    $this->message = 'OK';
+                }
+                socket_close($socket);
+            }
+        } else {
+            $this->success = FALSE;
+            $this->message = 'Input is missing!';
+        }
+        return $this->success;
+    }
+    
+    // Pause
+    function pause()
+    {
+        $this->reset_properties();
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if ($socket === false) {
+            $this->success = FALSE;
+            $this->message = socket_strerror(socket_last_error());
+            $this->message = iconv('CP1251', 'UTF-8', $this->message);
+        } else {
+            $result = @socket_connect($socket, $this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
+            if ($result === false) {
+                $this->success = FALSE;
+                $this->message = socket_strerror(socket_last_error($socket));
+                $this->message = iconv('CP1251', 'UTF-8', $this->message);
+            } else {
+                $packet = 'pause';
+                socket_write($socket, $packet, strlen($packet));
+                $this->success = TRUE;
+                $this->message = 'OK';
+            }
+            socket_close($socket);
+        }
+        return $this->success;
+    }
+    
+    // Stop
+    function stop()
+    {
+        return $this->pause();
+    }
+    
+    // Default command
+    function command($command, $parameter)
+    {
+        $this->reset_properties();
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if ($socket === false) {
+            $this->success = FALSE;
+            $this->message = socket_strerror(socket_last_error());
+            $this->message = iconv('CP1251', 'UTF-8', $this->message);
+        } else {
+            $result = @socket_connect($socket, $this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
+            if ($result === false) {
+                $this->success = FALSE;
+                $this->message = socket_strerror(socket_last_error($socket));
+                $this->message = iconv('CP1251', 'UTF-8', $this->message);
+            } else {
+                $packet = $command . (strlen($parameter) ? ':' . $parameter : '');
+                socket_write($socket, $packet, strlen($packet));
+                $this->success = TRUE;
+                $this->message = 'OK';
+            }
+            socket_close($socket);
+        }
+        return $this->success;
+    }
+    
 }
 
 ?>
