0f8f190e00c7ebe6be42e6b5b7d52be5b0827a60
tarasfrompir <tarasfrompir@inbox.ru>
2019-06-12 10:36:15 +0300

    Update terminals.class.php

--- modules/terminals/terminals.class.php
+++ modules/terminals/terminals.class.php
@@ -207,7 +207,7 @@ class terminals extends module
                 sayToTextSafe($terminal['NAME'], $details['event']);
             }
             return 1;
-        } else if ($event == 'SAY_CACHED_READY' AND $details['level'] >= (int) getGlobal('minMsgLevel')) {
+/*         } else if ($event == 'SAY_CACHED_READY' AND $details['level'] >= (int) getGlobal('minMsgLevel')) {
             
             
             // берем длинну сообщения
@@ -243,7 +243,7 @@ class terminals extends module
             } else {
                 $terminals = getTerminalsByCANTTS();
             }
-            $this->terminalSayByCacheQueue($terminals, $details);
+            $this->terminalSayByCacheQueue($terminals, $details); */
         } else if ($event == 'HOURLY') {
             // check terminals
             SQLExec('UPDATE terminals SET IS_ONLINE=0 WHERE LATEST_ACTIVITY < (NOW() - INTERVAL 60 MINUTE)');
