08f70d06d0cc152b893485180d8678f42f748745
tarasfrompir <tarasfrompir@inbox.ru>
2019-06-21 11:55:16 +0300

    Update cycle_terminals.php

--- scripts/cycle_terminals.php
+++ scripts/cycle_terminals.php
@@ -23,49 +23,35 @@ echo date("H:i:s") . " running " . basename(__FILE__) . PHP_EOL;
 
 setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
 
-while (1)
-{
-   if (time() - $checked_time > 30)
-   {
-      $checked_time = time();
-      setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
-   }
-
+while (1) {
+    if (time() - $checked_time > 30) {
+        $checked_time = time();
+        setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
+    }
+    
     $messages = SQLSelect("SELECT * FROM shouts WHERE SOURCE LIKE '%^%' ORDER BY ID ASC");
-	if ($messages) {
-		foreach ($messages as $message) {
-			$out_terminals = explode("^", $message['SOURCE']);	
-			foreach ($out_terminals as $terminals) {
-				$terminal = SQLSelectOne("SELECT * FROM terminals WHERE ID = '" . $terminals . "'");
-				// проверяем чтобы был файл и function sayttotext и отправляем
+    if ($messages) {
+        foreach ($messages as $message) {
+            $out_terminals = explode("^", $message['SOURCE']);
+            foreach ($out_terminals as $terminals) {
+                $terminal = SQLSelectOne("SELECT * FROM terminals WHERE ID = '" . $terminals . "'");
+                // запускаем все что имеет function sayttotext
                 if (file_exists(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php')) {
-					if (strpos(file_get_contents(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php'), "function sayttotext")) {
-						include_once(DIR_MODULES . 'app_player/addons.php');
-						include_once(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php');
-						if (class_exists($terminal['PLAYER_TYPE'])) {
-							if (is_subclass_of($terminal['PLAYER_TYPE'], 'app_player_addon', TRUE)) {
-								$player = new $terminal['PLAYER_TYPE']($terminal);
-							}
-						}
-						
-						$out = $player->sayttotext($message['MESSAGE'], $message['EVENT']);
-						while (!$out) {
-							$out = $player->sayttotext($message['MESSAGE'], $message['EVENT']);
-						}
-					}
-				}
-                // конец блока function sayttotext
-				
-				$message['SOURCE'] = str_replace($terminal['ID'] . '^', "", $message['SOURCE']);
-				SQLUpdate('shouts', $message); 
-			}
-		}
-	}
+                    if (strpos(file_get_contents(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php'), "function sayttotext")) {
+                        sayToTextSafe($message['ID'], $terminal['ID']);
+                    }
+                } else {
+                    // sleduyushiy tip terminalov
+                }
+                $message['SOURCE'] = str_replace($terminal['ID'] . '^', "", $message['SOURCE']);
+                SQLUpdate('shouts', $message);
+            }
+        }
+    }
     usleep(500000);
-	   if (file_exists('./reboot') || IsSet($_GET['onetime']))
-   {
-      exit;
-   }
+    if (file_exists('./reboot') || IsSet($_GET['onetime'])) {
+        exit;
+    }
 }
 
 DebMes("Unexpected close of cycle: " . basename(__FILE__));
