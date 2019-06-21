f78ff6d756f373cc6ca50a5f7f69956a4a11a4b6
tarasfrompir <tarasfrompir@inbox.ru>
2019-06-21 12:45:49 +0300

    Update telegramm.addon.php

--- modules/app_player/addons/telegramm.addon.php
+++ modules/app_player/addons/telegramm.addon.php
@@ -15,12 +15,12 @@ class telegramm extends app_player_addon
         $this->description .= ' Терминал имеет обратную связь с сайтом Телеграмм.(сообщения не теряются).';
         $this->terminal = $terminal;
         $this->reset_properties();
-        DebMes('Отписываем от событий терминал Телеграмма ' . microtime(true), 'terminals2');
+        //DebMes('Отписываем от событий терминал Телеграмма ' . microtime(true), 'terminals2');
         unsubscribeFromEvent('telegram', 'SAY');
         unsubscribeFromEvent('telegram', 'SAYTO');
         unsubscribeFromEvent('telegram', 'ASK');
         unsubscribeFromEvent('telegram', 'SAYREPLY');
-        DebMes('Отписались от событий терминал Телеграмма ' . microtime(true), 'terminals2');
+        //DebMes('Отписались от событий терминал Телеграмма ' . microtime(true), 'terminals2');
     }
     
     // Say
@@ -32,14 +32,14 @@ class telegramm extends app_player_addon
         if (file_exists(DIR_MODULES . 'telegram/telegram.class.php')) {
             $users   = SQLSelect("SELECT * FROM tlg_user ");
             $c_users = count($users);
-            DebMes('Проверяем количество пользователей в модуле Телеграма для сообщения ' . $message . ' ' . microtime(true), 'terminals2');
+            //DebMes('Проверяем количество пользователей в модуле Телеграма для сообщения ' . $message . ' ' . microtime(true), 'terminals2');
             if ($message AND $c_users) {
                 for ($j = 0; $j < $c_users; $j++) {
                     $user_id = $users[$j]['USER_ID'];
                     if ($user_id === '0') {
                         $user_id = $users[$j]['NAME'];
                     }
-                    DebMes('Отобрано пользователей в телеграме для сообщения ' . $message . ' ' . microtime(true), 'terminals2');
+                    //DebMes('Отобрано пользователей в телеграме для сообщения ' . $message . ' ' . microtime(true), 'terminals2');
                     $url = BASE_URL . "/ajax/telegram.html?sendMessage=1&user=" . $user_id . "&text=" . urlencode($message);
                     //$out = getURL($url,0);
                     $out = getURLBackground($url, 0);
