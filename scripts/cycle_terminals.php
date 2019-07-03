7a7b5a70a7d6374ef12c169daaa4849127375789
tarasfrompir <tarasfrompir@inbox.ru>
2019-07-03 15:50:31 +0300

    Update cycle_terminals.php

--- scripts/cycle_terminals.php
+++ scripts/cycle_terminals.php
@@ -24,8 +24,9 @@ echo date("H:i:s") . " running " . basename(__FILE__) . PHP_EOL;
 setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
 
 while (1) {
-    if (time() - $checked_time > 30) {
+    if (time() - $checked_time > 60) {
         $checked_time = time();
+		SQLExec("UPDATE shouts SET SOURCE='' WHERE ADDED< (NOW() - INTERVAL 5 MINUTE)");
         setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
     }
     // отправка только текстовых сообщений
@@ -34,52 +35,50 @@ while (1) {
         $out_terminals = explode("^", $message['SOURCE']);
         foreach ($out_terminals as $terminals) {
             $terminal = SQLSelectOne("SELECT * FROM terminals WHERE ID = '" . $terminals . "'");
-            // pinguem terminal
-            if (!$terminal['IS_ONLINE']) {
+			// pinguem terminal
+			if (!$terminal['IS_ONLINE'] ) {
                 pingTerminalSafe($terminal['NAME']);
             }
             //DebMes('Проверяем наличие файла для запуска отделный поток для терминала ' . $terminal['ID'] . ' ' . microtime(true), 'terminals2');
             // запускаем все что имеет function sayttotext
-            if (file_exists(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php')) {
-                if (strpos(file_get_contents(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php'), "function sayttotext")) {
-                    if ($terminal['IS_ONLINE'] AND $terminal['CANPLAY'] AND $terminal['CANTTS']) {
+            if (file_exists(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php') ) {
+                if (strpos(file_get_contents(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php'), "function sayttotext") ) {
+	                if ($terminal['IS_ONLINE']  AND $terminal['CANPLAY'] AND $terminal['CANTTS']) {
                         //DebMes('Запускаем очередь в отделный поток для soobcsheniya ' . $message['MESSAGE'] . ' ' . microtime(true), 'terminals2');
                         sayToTextSafe($message['ID'], $terminal['ID']);
                         //sayToText($message['ID'], $terminal['ID']);
                         //DebMes('Ochered zapushena для soobcsheniya ' . $message['MESSAGE'] . ' ' . microtime(true), 'terminals2');
-                    }
-                    $message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $message['SOURCE']);
+					}
+					$message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $message['SOURCE']);
                 }
-            }
-            
+			}
+
         }
         //processSubscriptionsSafe($message['EVENT'], array('level' => $message['IMPORTANCE'], 'message' => $message['MESSAGE'], 'id' => $message['ID']));
         SQLUpdate('shouts', $message);
     }
     // отправка сообщений сгенерированных ТТС
-    $message = SQLSelectOne("SELECT * FROM shouts WHERE SOURCE LIKE '%^%' AND FILE_LINK != '' ORDER BY ID ASC");
+	$message = SQLSelectOne("SELECT * FROM shouts WHERE SOURCE LIKE '%^%' AND FILE_LINK != '' ORDER BY ID ASC");
     if ($message) {
         $out_terminals = explode("^", $message['SOURCE']);
         foreach ($out_terminals as $terminals) {
             $terminal = SQLSelectOne("SELECT * FROM terminals WHERE ID = '" . $terminals . "'");
             //DebMes('Проверяем наличие файла для запуска отделный поток для терминала ' . $terminal['ID'] . ' ' . microtime(true), 'terminals2');
             // запускаем все что имеет function sayttotext
-            if (file_exists(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php') AND !gg($terminal['LINKED_OBJECT'] . '.BASY')) {
-                if (strpos(file_get_contents(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php'), "function sayToMedia")) {
-                    if ($terminal['IS_ONLINE'] AND $terminal['CANPLAY'] AND $terminal['CANTTS']) {
-                        sg($terminal['LINKED_OBJECT'] . '.BASY', 1);
+            if (file_exists(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php' )  AND !gg($terminal['LINKED_OBJECT'].'.BASY')) {
+                if (strpos(file_get_contents(DIR_MODULES . 'app_player/addons/' . $terminal['PLAYER_TYPE'] . '.addon.php'), "function sayToMedia") ) {
+					if ($terminal['IS_ONLINE']  AND $terminal['CANPLAY'] AND $terminal['CANTTS']) {
+						sg ($terminal['LINKED_OBJECT'].'.BASY',1);
                         sayTToMediaSafe($message['ID'], $terminal['ID']);
                         //sayToText($message['ID'], $terminal['ID']);
                         //DebMes('Ochered zapushena для soobcsheniya ' . $message['MESSAGE'] . ' ' . microtime(true), 'terminals2');
-                    }
+					}
                     $message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $message['SOURCE']);
-                }
+                } 
             }
         }
         SQLUpdate('shouts', $message);
     }
-    //DebMes( 'term id'.$terminal['ID']);
-    //DebMes(gg ('terminal4.BASY'));
     usleep(500000);
     if (file_exists('./reboot') || IsSet($_GET['onetime'])) {
         exit;
