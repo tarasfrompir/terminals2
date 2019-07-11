008df2112e5210c8f7fb595d5b30d9b53265325a
tarasfrompir <tarasfrompir@inbox.ru>
2019-07-11 12:29:27 +0300

    Update sendmesaagetoterminal2.php

--- lib/sendmesaagetoterminal2.php
+++ lib/sendmesaagetoterminal2.php
@@ -1,4 +1,14 @@
 <?php
+function remote_file_exists($url){
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_NOBODY, true);
+    curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    if( $httpCode == 200 ){return true;}
+    return false;
+}
+
 function send_message_to_terminal($terminal, $message, $event, $member, $level, $filename, $linkfile, $lang, $langfull, $timeshift)
 {
     if (!$terminal) {
