69d3e040788cdfe50d33c1c38ed7afe980bb89a3
tarasfrompir <tarasfrompir@inbox.ru>
2019-07-11 15:36:24 +0300

    Update xbmc.addon.php

--- modules/app_player/addons/xbmc.addon.php
+++ modules/app_player/addons/xbmc.addon.php
@@ -12,7 +12,7 @@ class xbmc extends app_player_addon {
 	function __construct($terminal) {
 		$this->title = 'KODI (XBMC) alicevox';
 		$this->description = 'Описание: Бесплатный кроссплатформенный медиаплеер и программное обеспечение для организации HTPC с открытым исходным кодом.'; 
-                $this->description .= 'Поддерживаемые возможности say, sayto, sayreply, ask через alicevox плагин для KODI (https://mjdm.ru/forum/viewtopic.php?f=5&t=2893&start=120).'; 
+        $this->description .= 'Поддерживаемые возможности say, sayto, sayreply, ask через alicevox плагин для KODI (https://mjdm.ru/forum/viewtopic.php?f=5&t=2893&start=120).'; 
 		
 		$this->terminal = $terminal;
 		$this->reset_properties();
@@ -222,33 +222,50 @@ class xbmc extends app_player_addon {
 		return $this->success;
 	}
 	
-    function sayToMedia($message_link, $time_message) //SETTINGS_SITE_LANGUAGE_CODE=код языка
+	// Say
+   function sayToMedia($message_link, $time_message) //SETTINGS_SITE_LANGUAGE_CODE=код языка
     {
+		// преобразовываем файл в вав формат
+		$path_parts = pathinfo($message_link);
+        if ($path_parts['extension'] != 'wav') {
+			if (!defined('PATH_TO_FFMPEG')) {
+				if (IsWindowsOS()) {
+					define("PATH_TO_FFMPEG", SERVER_ROOT . '/apps/ffmpeg/ffmpeg.exe');
+				} else {
+					define("PATH_TO_FFMPEG", 'ffmpeg');
+				}
+				$outlink = str_ireplace(".".$path_parts['extension'] , ".wav", $message_link);
+				$out = shell_exec(PATH_TO_FFMPEG . " -i " . $message_link . " -acodec pcm_s16le -ar 44100 ".$outlink." 2>&1");
+			}
+		}
+		DebMes(PATH_TO_FFMPEG . " -i " . $message_link . " -acodec pcm_s16le -ar 44100 ".$outlink." 2>&1");
         
         // берем ссылку http
-        if (preg_match('/\/cms\/cached.+/', $message_link, $m)) {
+        if (preg_match('/\/cms\/cached.+/', $outlink, $m)) {
             $server_ip = getLocalIp();
             if (!$server_ip) {
                 DebMes("Server IP not found", 'terminals');
                 return false;
             } else {
-                $message_link = 'http://' . $server_ip . $m[0];
+                $outlink = 'http://' . $server_ip . $m[0];
             }
         }
+		
         //  в некоторых системах есть по несколько серверов, поэтому если файл отсутствует, то берем путь из BASE_URL
-        if (!remote_file_exists($message_link)) {
-            $message_link = BASE_URL . $m[0];
+        if (!remote_file_exists($outlink)) {
+            $outlink = BASE_URL . $m[0];
         }
-	if(strlen($message_link)) {
-		if($this->kodi_request('Addons.ExecuteAddon', array('addonid'=>'script.alicevox.master', 'params'=>array($message_link)))) {
-			$this->success = TRUE;
-			$this->message = 'OK';
+
+		if(strlen($outlink)) {
+			if($this->kodi_request('Addons.ExecuteAddon', array('addonid'=>'script.alicevox.master', 'params'=>array($outlink)))) {
+				$this->success = TRUE;
+				$this->message = 'OK';
+			}
+		} else {
+			$this->success = FALSE;
+			$this->message = 'Input is missing!';
 		}
-	} else {
-		$this->success = FALSE;
-		$this->message = 'Input is missing!';
-	}
-	return $this->success;
+		return $this->success;
 	}
 	
 	// Pause
@@ -559,4 +576,3 @@ class xbmc extends app_player_addon {
 	
 }
 ?>
-
