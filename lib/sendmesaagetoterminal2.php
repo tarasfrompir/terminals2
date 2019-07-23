72763120d907a0883840c69efe165d5f259531dc
tarasfrompir <tarasfrompir@inbox.ru>
2019-07-23 22:52:53 +0300

    Update sendmesaagetoterminal2.php

--- lib/sendmesaagetoterminal2.php
+++ lib/sendmesaagetoterminal2.php
@@ -1,14 +1,4 @@
 <?php
-function remote_file_exists($url){
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_NOBODY, true);
-    curl_exec($ch);
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    if( $httpCode == 200 ){return true;}
-    return false;
-}
-
 function get_remote_filesize($url)
 {
     $head = array_change_key_case(get_headers($url, 1));
