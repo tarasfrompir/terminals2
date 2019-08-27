24c5852d95e91242989b29eac628760ad0f8cfbc
tarasfrompir <tarasfrompir@inbox.ru>
2019-08-27 21:34:45 +0300

    Update chromecast.addon.php

--- modules/app_player/addons/chromecast.addon.php
+++ modules/app_player/addons/chromecast.addon.php
@@ -33,10 +33,12 @@ class chromecast extends app_player_addon
         $loop     = FALSE;
         $repeat   = FALSE;
         
-        $cc            = new GChromecast($this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
+        $cc = new GChromecast($this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
         $cc->requestId = time();
-        $result        = $cc->getMediaSession();
-        DebMes($result);
+        $status = $cc->getStatus();
+        $cc->requestId = time();
+        $result = $cc->getMediaSession();
+        //DebMes($result);
         if ($result) {
             $this->reset_properties();
             $this->success = TRUE;
@@ -46,7 +48,7 @@ class chromecast extends app_player_addon
                 'length' => (int) $result['status'][0]['media']['duration'], //Track length in seconds. Integer. If unknown = 0. 
                 'time' => (int) $result['status'][0]['currentTime'], //Current playback progress (in seconds). If unknown = 0. 
                 'state' => (string) strtolower($result['status'][0]['playerState']), //Playback status. String: stopped/playing/paused/unknown 
-                'volume' => (int) $result['status'][0]['volume']['level']*100, // Volume level in percent. Integer. Some players may have values greater than 100.
+                'volume' => intval($status['status']['volume']['level']*100), // Volume level in percent. Integer. Some players may have values greater than 100.
                 'muted' => (int) $result['status'][0]['volume']['muted'], // Volume level in percent. Integer. Some players may have values greater than 100.
                 'random' => (boolean) $random, // Random mode. Boolean. 
                 'loop' => (boolean) $loop, // Loop mode. Boolean.
