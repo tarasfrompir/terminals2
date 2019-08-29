57ea34d1ebadb753a7bf8fc1440a17796cf0cc13
tarasfrompir <tarasfrompir@inbox.ru>
2019-08-30 01:21:32 +0300

    Update cycle_terminals.php

--- scripts/cycle_terminals.php
+++ scripts/cycle_terminals.php
@@ -60,7 +60,7 @@ while (1) {
 			continue;
 		}
 		$terminal = SQLSelectOne("SELECT * FROM terminals WHERE LINKED_OBJECT = '" . $terminals . "'");
-		$old_message = SQLSelectOne("SELECT * FROM shouts WHERE ID <= '" . $number_message ."' AND SOURCE LIKE '%" . $terminal['ID'] . "^%' AND CHEKED = '1' ORDER BY ID ASC");
+		$old_message = SQLSelectOne("SELECT * FROM shouts WHERE ID <= '" . $number_message ."' AND SOURCE LIKE '%" . $terminal['ID'] . "^%' ORDER BY ID ASC");
 	
 		// запускаем все что имеет function sayttotext
 		if ($old_message) {
