736bf20dc6573053b31db01a0bb890300d7e7763
tarasfrompir <tarasfrompir@inbox.ru>
2019-08-30 23:27:20 +0300

    Update googlehomenotifier.addon.php

--- modules/terminals/tts/googlehomenotifier.addon.php
+++ modules/terminals/tts/googlehomenotifier.addon.php
@@ -7,16 +7,25 @@ class googlehomenotifier extends tts_addon {
         parent::__construct($terminal);
     }
 
-    function say($phrase, $level = 0)
+    // Say
+    public function say_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка
     {
-        $port = $this->terminal['PLAYER_PORT'];
-        $language = SETTINGS_SITE_LANGUAGE;
-        if (!$port) {
-            $port = '8091';
+        if($message['MESSAGE']) {
+            $port = $this->terminal['PLAYER_PORT'];
+            $language = SETTINGS_SITE_LANGUAGE;
+            if (!$port) {
+                $port = '8091';
+            }
+            $host = $this->terminal['HOST'];
+            $url = 'http://' . $host . ':' . $port . '/google-home-notifier?language=' . $language . '&text=' . urlencode($phrase);
+            getURL($url, 0);
+        	usleep (200000);
+            $this->success = TRUE;
+            $this->message = 'OK';
+        } else {
+            $this->success = FALSE;
+            $this->message = 'Input is missing!';
         }
-        $host = $this->terminal['HOST'];
-        $url = 'http://' . $host . ':' . $port . '/google-home-notifier?language=' . $language . '&text=' . urlencode($phrase);
-        getURL($url, 0);
+        return $this->success;
     }
-
 }
