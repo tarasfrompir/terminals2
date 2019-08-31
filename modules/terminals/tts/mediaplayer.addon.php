537e011b9c6a61e15442a6ccb74e74a25461bcde
tarasfrompir <tarasfrompir@inbox.ru>
2019-08-31 13:48:36 +0300

    Update mediaplayer.addon.php

--- modules/terminals/tts/mediaplayer.addon.php
+++ modules/terminals/tts/mediaplayer.addon.php
@@ -7,23 +7,24 @@ class mediaplayer extends tts_addon
         $this->title = "MediaPlayer";
         parent::__construct($terminal);
     }
-
+    
     // Say
     public function say_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка
     {
-        if($message['CACHED_FILENAME']) {
-            if(file_exists($message['CACHED_FILENAME'])) {
+        if ($message['CACHED_FILENAME']) {
+            if (file_exists($message['CACHED_FILENAME'])) {
                 if (preg_match('/\/cms\/cached.+/', $message['CACHED_FILENAME'], $m)) {
                     $message['CACHED_FILENAME'] = 'http://' . getLocalIp() . $m[0];
+                    stopMedia($terminal['HOST']);
                     setPlayerVolume($terminal['HOST'], $terminal['MESSAGE_VOLUME_LEVEL']);
                     playMedia($message['CACHED_FILENAME'], $terminal['NAME']);
-                    sleep ($message['MESSAGE_DURATION']);
+                    sleep($message['MESSAGE_DURATION']);
                     $this->success = TRUE;
                     $this->message = 'OK';
-				} else {
-		            $this->success = FALSE;
+                } else {
+                    $this->success = FALSE;
                     $this->message = 'Input is missing!';
-				}
+                }
             } else {
                 $this->success = FALSE;
                 $this->message = 'Command execution error!';
@@ -33,5 +34,5 @@ class mediaplayer extends tts_addon
             $this->message = 'Input is missing!';
         }
         return $this->success;
-   }
+    }
 }
