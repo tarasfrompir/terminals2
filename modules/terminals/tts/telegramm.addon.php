7340530b654fc909fa1dece7ce1bb5812d45b4b5
tarasfrompir <tarasfrompir@inbox.ru>
2019-08-31 17:22:59 +0300

    Add files via upload

---
+++ modules/terminals/tts/telegramm.addon.php
@@ -0,0 +1,45 @@
+<?php
+
+class telegramm extends tts_addon {
+
+    function __construct($terminal) {
+        $this->title="Telegramm module";
+        unsubscribeFromEvent('telegram', 'SAY');
+        unsubscribeFromEvent('telegram', 'SAYTO');
+        unsubscribeFromEvent('telegram', 'ASK');
+        unsubscribeFromEvent('telegram', 'SAYREPLY');
+        parent::__construct($terminal);
+    }
+
+    // Say
+    function say_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка
+    {
+		DebMes($message['MESSAGE']);
+        if (file_exists(DIR_MODULES . 'telegram/telegram.class.php')) {
+            $users   = SQLSelect("SELECT * FROM tlg_user ");
+            $c_users = count($users);
+            if ($message['MESSAGE'] AND $c_users) {
+                for ($j = 0; $j < $c_users; $j++) {
+                    $user_id = $users[$j]['USER_ID'];
+                    if ($user_id === '0') {
+                        $user_id = $users[$j]['NAME'];
+                    }
+                    $url = BASE_URL . "/ajax/telegram.html?sendMessage=1&user=" . $user_id . "&text=" . urlencode($message['MESSAGE']);
+                    getURLBackground($url,0);
+                    usleep(100000);
+	                $this->success = TRUE;
+                    $this->message = 'OK';
+				}
+            } else {
+                $this->success = FALSE;
+                $this->message = 'Command execution error!';
+            }
+        } else {
+            $this->success = FALSE;
+            $this->message = 'Input is missing!';
+        }
+        return $this->success;
+    }
+}
+
+?>
