96d7dd2e9328b87a3407313d7324fd4391dc9ae5
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-03 22:30:58 +0300

    Update cycle_terminals.php

--- scripts/cycle_terminals.php
+++ scripts/cycle_terminals.php
@@ -17,8 +17,13 @@ echo date("H:i:s") . " Init module " . PHP_EOL;
 $checked_time = 0;
 
 // set all terminal as free when restart cycle
-$terminals = getObjectsByProperty('basy', '==', '1');
-foreach ($terminals as $terminal) {
+$terminalss = getObjectsByProperty('basy', '==', '1');
+foreach ($terminalss as $terminals) {
+	if (!$terminals ) {
+		echo serialize($terminal);
+        continue;
+	}
+    $terminal = SQLSelectOne("SELECT * FROM terminals WHERE LINKED_OBJECT = '" . $terminals . "' LIMIT 1");
     sg($terminal['LINKED_OBJECT'] . '.basy', 0);
 }
 
@@ -28,7 +33,7 @@ $number_message = $number_message['ID'] + 1;
 DebMes('Start terminals cycle');
 while (1) {
     // time update cicle of terminal
-    if (time() - $checked_time > 60) {
+    if (time() - $checked_time > 10) {
         $checked_time = time();
         setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
     }
@@ -49,13 +54,14 @@ while (1) {
     // chek all old message and send message to terminals
     $out_terminals = getObjectsByProperty('basy', '==', '0');
     foreach ($out_terminals as $terminals) {
-        if (!$terminals) {
-            continue;
-        }
+        if (!$terminals ) {
+			usleep(200000);
+            break;
+		}
         $terminal = SQLSelectOne("SELECT * FROM terminals WHERE LINKED_OBJECT = '" . $terminals . "' LIMIT 1");
         $old_message = SQLSelectOne("SELECT * FROM shouts WHERE ID <= '" . $number_message . "' AND SOURCE LIKE '%" . $terminal['ID'] . "^%' ORDER BY ID ASC LIMIT 1");
         // если есть сообщение для этого терминала то пускаем его
-        if ($old_message) {
+        if ($old_message['ID'] AND $terminal['ONLINE'] == 1) {
 			// убираем запись айди терминала из таблицы шутс - если не воспроизведется то вернет эту запись функция send_message($old_message, $terminal);
             $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
             SQLUpdate('shouts', $old_message);
@@ -68,6 +74,11 @@ while (1) {
             }
             sg($terminal['LINKED_OBJECT'] . '.basy', 1);
             send_messageSafe($old_message, $terminal);
+			usleep(200000);
+        } else if ($old_message['ID'] AND $terminal['ONLINE'] == 0) {
+            $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
+            SQLUpdate('shouts', $old_message);
+			usleep(200000);
         } else if ($restored_info = json_decode(gg($terminal['LINKED_OBJECT'] . '.playerdata'), true) AND $terminal['TTS_TYPE'] == 'mediaplayer') {
             // inache vosstanavlivaem vosproizvodimoe
             stopMedia($terminal['HOST']);
@@ -75,6 +86,7 @@ while (1) {
             playMedia($restored_info['file'], $terminal['NAME']);
             seekPlayerPosition($terminal['NAME'], $restored_info['time']);
             sg($terminal['LINKED_OBJECT'] . '.playerdata', '');
+			usleep(200000);
         }
     }
     
@@ -84,3 +96,4 @@ while (1) {
 }
 
 DebMes("Unexpected close of cycle: " . basename(__FILE__));
+
