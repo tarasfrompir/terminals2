ba2e0a4529a27d8211ec7a7a7ba40ffcc2c766bb
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-06 13:13:48 +0300

    Update cycle_terminals.php

--- scripts/cycle_terminals.php
+++ scripts/cycle_terminals.php
@@ -9,7 +9,7 @@ echo date("H:i:s") . " Running " . basename(__FILE__) . PHP_EOL;
 echo date("H:i:s") . " Init module " . PHP_EOL;
 $checked_time = 0;
 // set all terminal as free when restart cycle
-$outs = getObjectsByProperty('basy', '==', '1');
+$outs         = getObjectsByProperty('basy', '==', '1');
 foreach ($outs as $out) {
     if (!$out) {
         continue;
@@ -38,54 +38,54 @@ while (1) {
     if ($message) {
         $number_message = $number_message + 1;
         DebMes("Max nomber message - " . $number_message, 'terminals');
-	} else {
+    } else {
         sleep(1);
     }
-	
-	$out_terminals = getObjectsByProperty('basy', '==', '0');
+    
+    $out_terminals = getObjectsByProperty('basy', '==', '0');
     foreach ($out_terminals as $terminals) {
         // если пустой терминал пропускаем
-		if (!$terminals ) {
+        if (!$terminals) {
             continue;
-	    }
-		$terminal = SQLSelectOne("SELECT * FROM terminals WHERE LINKED_OBJECT = '" . $terminals . "'");
+        }
+        $terminal    = SQLSelectOne("SELECT * FROM terminals WHERE LINKED_OBJECT = '" . $terminals . "'");
         $old_message = SQLSelectOne("SELECT * FROM shouts WHERE ID <= '" . $number_message . "' AND SOURCE LIKE '%" . $terminal['ID'] . "^%' ORDER BY ID ASC");
-		// если отсутствует сообщение пропускаем
-		if (!$old_message['MESSAGE']) {
+        // если отсутствует сообщение пропускаем
+        if (!$old_message['MESSAGE']) {
             continue;
-	    }
-		// если терминал оффлайн удаляем из работы эту запись и пропускаем (пингуется дополнительно - если вернется с ошибкой отправления)
-		if (!$terminal['IS_ONLINE'] ) {
-			$old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
+        }
+        // если терминал оффлайн удаляем из работы эту запись и пропускаем (пингуется дополнительно - если вернется с ошибкой отправления)
+        if (!$terminal['IS_ONLINE']) {
+            $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
             SQLUpdate('shouts', $old_message);
             continue;
-	    }
-		// если тип терминала воспроизводящий аудио и нету еще сгенерированного файла пропускаем 
+        }
+        // если тип терминала воспроизводящий аудио и нету еще сгенерированного файла пропускаем 
         if (($terminal['TTS_TYPE'] == 'mediaplayer' OR $terminal['TTS_TYPE'] == 'mainterminal') AND !$old_message['CACHED_FILENAME']) {
-	        continue;
-	    } else {
-			// иначе запускаем его воспроизведение
+            continue;
+        } else {
+            // иначе запускаем его воспроизведение
             // убираем запись айди терминала из таблицы шутс - если не воспроизведется то вернет эту запись функция send_message($old_message, $terminal);
             $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
             SQLUpdate('shouts', $old_message);
-			//записываем что терминал занят
+            //записываем что терминал занят
             sg($terminal['LINKED_OBJECT'] . '.basy', 1);
-			//передаем сообщение на терминалы воспроизводящие аудио
+            //передаем сообщение на терминалы воспроизводящие аудио
             send_messageSafe($old_message, $terminal);
-		}
-		// если тип терминала передающий только текстовое сообщение  
+        }
+        // если тип терминала передающий только текстовое сообщение  
         if (!$terminal['TTS_TYPE'] == 'mediaplayer' OR !$terminal['TTS_TYPE'] == 'mainterminal') {
-  			// запускаем его воспроизведение
+            // запускаем его воспроизведение
             // убираем запись айди терминала из таблицы шутс - если не воспроизведется то вернет эту запись функция send_message($old_message, $terminal);
             $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
             SQLUpdate('shouts', $old_message);
-			//записываем что терминал занят
+            //записываем что терминал занят
             sg($terminal['LINKED_OBJECT'] . '.basy', 1);
-			//передаем сообщение на терминал передающий только текстовое сообщение 
+            //передаем сообщение на терминал передающий только текстовое сообщение 
             send_messageSafe($old_message, $terminal);
-	    }		
-	}
-
+        }
+    }
+    
     
     if (file_exists('./reboot') || IsSet($_GET['onetime'])) {
         exit;
