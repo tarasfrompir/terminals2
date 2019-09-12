8e76d45aba8fd10c7861ada804d95a49fff03e02
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-12 14:51:17 +0300

    Update cycle_terminals.php

--- scripts/cycle_terminals.php
+++ scripts/cycle_terminals.php
@@ -7,19 +7,30 @@ set_time_limit(0);
 include_once("./load_settings.php");
 echo date("H:i:s") . " Running " . basename(__FILE__) . PHP_EOL;
 echo date("H:i:s") . " Init module " . PHP_EOL;
+include_once(DIR_MODULES . "terminals/terminals.class.php");
+$ter = new terminals();
 $checked_time = 0;
 // set all terminal as free when restart cycle
-$term         = SQLSelect("SELECT * FROM terminals");
+$term = SQLSelect("SELECT * FROM terminals");
 foreach ($term as $t) {
     sg($t['LINKED_OBJECT'] . '.BASY', 0);
 }
-
 //SQLExec("UPDATE shouts SET SOURCE = '' ");
 // get number last message
 $number_message = SQLSelectOne("SELECT * FROM shouts ORDER BY ID DESC");
 $number_message = $number_message['ID'] + 1;
-DebMes("Max nomber message - " . $number_message, 'terminals');
+DebMes("Start number message - " . $number_message, 'terminals');
 DebMes('Start terminals cycle');
+
+// берем конфигурацию с модуля терминалов
+$ter->getConfig();
+if ($ter->config['TERMINALS_TIMEOUT']) {
+	$terminals_time_out = $ter->config['TERMINALS_TIMEOUT'];
+} else {
+    $terminals_time_out = 10;
+}
+if ($ter->config['LOG_ENABLED']) DebMes("Get timeout for message - " . $terminals_time_out. " minutes", 'terminals');
+
 while (1) {
     // time update cicle of terminal
     if (time() - $checked_time > 10) {
@@ -27,31 +38,39 @@ while (1) {
         setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
     }
     //время жизни сообщений
-    if (time() - $clear_message > 60) {
+    if (time() - $clear_message > 60*$terminals_time_out) {
         $clear_message = time();
-        SQLExec("UPDATE shouts SET SOURCE = '' WHERE ADDED < (NOW() - INTERVAL 1 MINUTE)");
+        SQLExec("UPDATE shouts SET SOURCE = '' WHERE ADDED < (NOW() - INTERVAL ".$terminals_time_out." MINUTE)");
+		// когда в настройках терминалов изменили таймаут для сообщений то получаем по новой
+		$ter->getConfig();
+		if (!$ter->config['TERMINALS_TIMEOUT'] ) {
+			$terminals_time_out = 10;
+		} else if ($ter->config['TERMINALS_TIMEOUT'] != $terminals_time_out ) {
+			$terminals_time_out = $ter->config['TERMINALS_TIMEOUT'];
+	        if ($ter->config['LOG_ENABLED']) DebMes("Change timeout for message to " . $terminals_time_out. " minutes", 'terminals');
+		}
     }
     // CHEK next message for terminals ready
     $message = SQLSelectOne("SELECT 1 FROM shouts WHERE ID = '" . $number_message . "'");
     if ($message) {
         $number_message = $number_message + 1;
-        DebMes("Max nomber message - " . $number_message, 'terminals');
+        if ($ter->config['LOG_ENABLED']) DebMes("Max nomber message - " . $number_message, 'terminals');
     } else {
         sleep(1);
     }
     
     $out_terminals = getObjectsByProperty('basy', '==', '0');
-    //DebMes("Array of free terminal ".serialize($out_terminals) , 'terminals');
+    if ($ter->config['LOG_ENABLED']) DebMes("Array of free terminal ".serialize($out_terminals) , 'terminals');
     foreach ($out_terminals as $terminals) {
         // если пустой терминал пропускаем
         if (!$terminals) {
-            DebMes("No free terminal " . $terminals, 'terminals');
+            if ($ter->config['LOG_ENABLED']) DebMes("No free terminal " . $terminals, 'terminals');
             continue;
         }
         $terminal = SQLSelectOne("SELECT * FROM terminals WHERE LINKED_OBJECT = '" . $terminals . "'");
         // если пустая инфа о терминале пропускаем
         if (!$terminal) {
-            DebMes("No information of terminal" . $terminal['NAME'], 'terminals');
+            if ($ter->config['LOG_ENABLED']) DebMes("No information of terminal" . $terminal['NAME'], 'terminals');
             continue;
         }
         $old_message = SQLSelectOne("SELECT * FROM shouts WHERE ID <= '" . $number_message . "' AND SOURCE LIKE '%" . $terminal['ID'] . "^%' ORDER BY ID ASC");
@@ -60,19 +79,19 @@ while (1) {
             try {
                 $restored = json_decode(gg($terminal['LINKED_OBJECT'] . '.playerdata'), true);
                 if (is_array($restored)) {
-                    DebMes("Restore volume on the terminal - " . $terminal['NAME'], 'terminals');
+                    if ($ter->config['LOG_ENABLED']) DebMes("Restore volume on the terminal - " . $terminal['NAME'], 'terminals');
                     setPlayerVolume($terminal['NAME'], $restored['volume']);
                     // если есть файл для воспроизведения то тоже его восстанавливаем
                     if ($restored['file']) {
                         playMedia($restored['file'], $terminal['NAME']);
-                        DebMes("Restore media on the terminal - " . $terminal['NAME'], 'terminals');
+                        if ($ter->config['LOG_ENABLED']) DebMes("Restore media on the terminal - " . $terminal['NAME'], 'terminals');
                     }
                 }
                 sg($terminal['LINKED_OBJECT'] . '.playerdata', '');
                 continue;
             }
             catch (Exception $e) {
-                DebMes("Error with restore playaed - " . $terminal['NAME'], 'terminals');
+                if ($ter->config['LOG_ENABLED']) DebMes("Error with restore playaed - " . $terminal['NAME'], 'terminals');
             }
         }
         // для остальных плееров просто пропускаем итерацию и при отсутствии сообщения 
@@ -83,7 +102,7 @@ while (1) {
         if (!$terminal['IS_ONLINE']) {
             $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
             SQLUpdate('shouts', $old_message);
-            DebMes("Disable message - " . $terminal['NAME'], 'terminals');
+            if ($ter->config['LOG_ENABLED']) DebMes("Disable message - " . $terminal['NAME'], 'terminals');
             continue;
         }
         // если тип терминала воспроизводящий аудио и нету еще сгенерированного файла пропускаем 
@@ -98,7 +117,7 @@ while (1) {
             sg($terminal['LINKED_OBJECT'] . '.basy', 1);
             //передаем сообщение на терминалы воспроизводящие аудио
             send_messageSafe($old_message, $terminal);
-            DebMes("Send message - " . $terminal['NAME'], 'terminals');
+            if ($ter->config['LOG_ENABLED']) DebMes("Send message - " . $terminal['NAME'], 'terminals');
             continue;
         }
         // если тип терминала передающий только текстовое сообщение  
@@ -111,7 +130,7 @@ while (1) {
             sg($terminal['LINKED_OBJECT'] . '.basy', 1);
             //передаем сообщение на терминал передающий только текстовое сообщение 
             send_messageSafe($old_message, $terminal);
-            DebMes("Send message - " . $terminal['NAME'], 'terminals');
+            if ($ter->config['LOG_ENABLED']) DebMes("Send message - " . $terminal['NAME'], 'terminals');
             continue;
         }
     }
