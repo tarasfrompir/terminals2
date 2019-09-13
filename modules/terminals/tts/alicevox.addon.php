ded7681d4b5978832ac15b689f7adc30070727ab
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-13 08:06:33 +0300

    Update alicevox.addon.php

--- modules/terminals/tts/alicevox.addon.php
+++ modules/terminals/tts/alicevox.addon.php
@@ -6,15 +6,15 @@ class alicevox extends tts_addon
 {
     function __construct($terminal)
     {
-        $this->title = "Alicevox";
-		$this->curl = curl_init();
-		$this->address = 'http://192.18.1.51:8080';
-		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($this->curl, CURLOPT_CONNECTTIMEOUT, 5);
-		curl_setopt($this->curl, CURLOPT_TIMEOUT, 5);
-		curl_setopt($this->curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-		curl_setopt($this->curl, CURLOPT_USERPWD, 'xbmc:xbmc');
-
+        $this->title   = "Alicevox";
+        $this->curl    = curl_init();
+        $this->address = 'http://192.18.1.51:8080';
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($this->curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($this->curl, CURLOPT_TIMEOUT, 5);
+        curl_setopt($this->curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($this->curl, CURLOPT_USERPWD, 'xbmc:xbmc');
+        
         parent::__construct($terminal);
     }
     
@@ -25,18 +25,28 @@ class alicevox extends tts_addon
         if ($message['CACHED_FILENAME']) {
             if (file_exists($message['CACHED_FILENAME'])) {
                 if (preg_match('/\/cms\/cached.+/', $message['CACHED_FILENAME'], $m)) {
-					include_once(DIR_MODULES . "app_player/addons/kodi.addon.php");
-                    $kodi = new kodi();
+                    include_once(DIR_MODULES . "app_player/addons/kodi.addon.php");
+                    $kodi                       = new kodi();
                     $message['CACHED_FILENAME'] = 'http://' . getLocalIp() . $m[0];
-		    $json = array('jsonrpc' => '2.0', 'method' => 'Addons.ExecuteAddon', 'params' => array('addonid' => 'script.alicevox.master','params' => array($message['CACHED_FILENAME'])), 'id' => (int)$['ID']);
-		    $request = json_encode($json);
-                    curl_setopt($this->curl, CURLOPT_URL, $this->address.'/jsonrpc?request='.urlencode($request));
-
-		    if($result = curl_exec($this->curl)) {
-			$code = curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
+                    $json = array(
+                        'jsonrpc' => '2.0',
+                        'method' => 'Addons.ExecuteAddon',
+                        'params' => array(
+                            'addonid' => 'script.alicevox.master',
+                            'params' => array(
+                                $message['CACHED_FILENAME']
+                            )
+                        ),
+                        'id' => (int) $message['ID']
+                    );
+                    $request = json_encode($json);
+                    curl_setopt($this->curl, CURLOPT_URL, $this->address . '/jsonrpc?request=' . urlencode($request));
+                    
+                    if ($result = curl_exec($this->curl)) {
+                        $code = curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
                     }
-		    if ($code==200 ) {
-			//vse ok
+                    if ($code == 200) {
+                        //vse ok
                     }
                     sleep($message['MESSAGE_DURATION']);
                     $this->success = TRUE;
@@ -56,4 +66,3 @@ class alicevox extends tts_addon
         return $this->success;
     }
 }
-
