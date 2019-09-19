174d72c7827017c2dbe4c75bb593e6133adab012
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-19 21:06:37 +0300

    Update majordroid_tts.addon.php

--- modules/terminals/tts/majordroid_tts.addon.php
+++ modules/terminals/tts/majordroid_tts.addon.php
@@ -3,9 +3,10 @@
 class majordroid_tts extends tts_addon {
 
     function __construct($terminal) {
-		$this->terminal = $terminal;
+        $this->terminal = $terminal;
         $this->title="MajorDroid";
-		$this->description = 'Используется на устройствах которые имеют в себе MajorDroid API.';
+        $this->description = 'Используется на устройствах которые имеют в себе MajorDroid API.';
+        $this->port = (empty($this->terminal['TTS_PORT'])?7999:$this->terminal['TTS_PORT']);
         parent::__construct($terminal);
     }
 
@@ -16,20 +17,16 @@ class majordroid_tts extends tts_addon {
 
     function sendMajorDroidCommand($cmd) {
         if ($this->terminal['HOST']) {
-            DebMes("Sending $cmd to ".$this->terminal['HOST'],'majordroid');
-            $service_port = '7999';
-            $in = $cmd;
-            $address = $this->terminal['HOST'];
             if (!preg_match('/^\d/', $address)) return 0;
             $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
             if ($socket === false) {
                 return 0;
             }
-            $result = socket_connect($socket, $address, $service_port);
+            $result = socket_connect($socket, $this->terminal['HOST'], $this->port);
             if ($result === false) {
                 return 0;
             }
-            $result = socket_write($socket, $in, strlen($in));
+            $result = socket_write($socket, $cmd, strlen($cmd));
             usleep(500000);
             socket_close($socket);
             return $result;
