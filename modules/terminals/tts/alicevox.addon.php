7bff34e3e34f01450b3246b0b5ced9557c859ba2
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-19 17:25:58 +0300

    Update alicevox.addon.php

--- modules/terminals/tts/alicevox.addon.php
+++ modules/terminals/tts/alicevox.addon.php
@@ -9,14 +9,8 @@ class alicevox extends tts_addon
 		$this->terminal = $terminal;
         $this->title   = "Alicevox";
 		$this->description = 'Работает на ХВМС устройствах с установленным скриптом  Аливокс.';
-        $this->curl    = curl_init();
-        $this->address = 'http://192.18.1.51:8080';
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($this->curl, CURLOPT_CONNECTTIMEOUT, 5);
-        curl_setopt($this->curl, CURLOPT_TIMEOUT, 5);
-        curl_setopt($this->curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($this->curl, CURLOPT_USERPWD, 'xbmc:xbmc');
-        
+        $this->address = '192.168.1.51:8080';
+
         parent::__construct($terminal);
     }
     
@@ -28,24 +22,12 @@ class alicevox extends tts_addon
             if (file_exists($message['CACHED_FILENAME'])) {
                 if (preg_match('/\/cms\/cached.+/', $message['CACHED_FILENAME'], $m)) {
                     $message['CACHED_FILENAME'] = 'http://' . getLocalIp() . $m[0];
-                    $json                       = array(
-                        'jsonrpc' => '2.0',
-                        'method' => 'Addons.ExecuteAddon',
-                        'params' => array(
-                            'addonid' => 'script.alicevox.master',
-                            'params' => array(
-                                $message['CACHED_FILENAME']
-                            )
-                        ),
-                        'id' => (int) $message['ID']
-                    );
-                    $request                    = json_encode($json);
-                    curl_setopt($this->curl, CURLOPT_URL, $this->address . '/jsonrpc?request=' . urlencode($request));
-                    
-                    if ($result = curl_exec($this->curl)) {
-                        $code = curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
-                    }
-                    if ($code == 200) {
+                    $url="http://xbmc:xbmc@".$this->address."/jsonrpc?request={\"jsonrpc\":\"2.0\",\"method\":\"Addons.ExecuteAddon\",\"params\":{\"addonid\":\"script.alicevox.master\",\"params\":[\"".$message['CACHED_FILENAME']."\"]},\"id\":1}";
+
+
+                    $result = getURL($url, 0);
+ 
+                    if ($result) {
                         sleep($message['MESSAGE_DURATION']);
                         $this->success = TRUE;
                         $this->message = 'OK';
