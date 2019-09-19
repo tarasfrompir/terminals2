57e4afe45f1f21cf6871d120918598d601a3465b
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-19 17:31:59 +0300

    Update alicevox.addon.php

--- modules/terminals/tts/alicevox.addon.php
+++ modules/terminals/tts/alicevox.addon.php
@@ -6,11 +6,9 @@ class alicevox extends tts_addon
 {
     function __construct($terminal)
     {
-		$this->terminal = $terminal;
+        $this->terminal = $terminal;
         $this->title   = "Alicevox";
-		$this->description = 'Работает на ХВМС устройствах с установленным скриптом  Аливокс.';
-        $this->address = '192.168.1.51:8080';
-
+        $this->description = 'Работает на ХВМС устройствах с установленным скриптом  Аливокс.';
         parent::__construct($terminal);
     }
     
@@ -22,7 +20,7 @@ class alicevox extends tts_addon
             if (file_exists($message['CACHED_FILENAME'])) {
                 if (preg_match('/\/cms\/cached.+/', $message['CACHED_FILENAME'], $m)) {
                     $message['CACHED_FILENAME'] = 'http://' . getLocalIp() . $m[0];
-                    $url="http://xbmc:xbmc@".$this->address."/jsonrpc?request={\"jsonrpc\":\"2.0\",\"method\":\"Addons.ExecuteAddon\",\"params\":{\"addonid\":\"script.alicevox.master\",\"params\":[\"".$message['CACHED_FILENAME']."\"]},\"id\":1}";
+                    $url="http://xbmc:xbmc@".$this->terminal['HOST'].":".$this->terminal['TTS_PORT']."/jsonrpc?request={\"jsonrpc\":\"2.0\",\"method\":\"Addons.ExecuteAddon\",\"params\":{\"addonid\":\"script.alicevox.master\",\"params\":[\"".$message['CACHED_FILENAME']."\"]},\"id\":1}";
 
 
                     $result = getURL($url, 0);
