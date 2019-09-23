ef09fea563381bfa4ba1fabef1a9fffa371f45e7
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-23 17:04:34 +0300

    Update alicevox.addon.php

--- modules/terminals/tts/alicevox.addon.php
+++ modules/terminals/tts/alicevox.addon.php
@@ -8,7 +8,7 @@ class alicevox extends tts_addon
     {
         $this->terminal = $terminal;
         $this->title   = "Alicevox";
-        $this->description = 'Работает на медиацентрах KODI  с установленным плагином  Аливокс. Ссылка на плагин - https://github.com/SergMicar/script.alicevox.master Ссылка на тему форума - https://mjdm.ru/forum/viewtopic.php?f=4&t=3765&p=88392&hilit=alicevox#p88392' ;
+        $this->description = 'Работает на медиацентрах KODI  с установленным плагином  Аливокс. Ссылка на плагин - https://github.com/SergMicar/script.alicevox.master Ссылка на тему форума - https://mjdm.ru/forum/viewtopic.php?f=5&t=2893&start=120' ;
         $this->address = 'http://xbmc:xbmc@'.$this->terminal['HOST'].':'.(empty($this->terminal['TTS_PORT'])?8080:$this->terminal['TTS_PORT']);
         parent::__construct($terminal);
     }
