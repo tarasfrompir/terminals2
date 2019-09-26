a36a2e35779012da2878096fce6f2030d14ee997
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-26 17:47:31 +0300

    Update cycle_terminals.php

--- scripts/cycle_terminals.php
+++ scripts/cycle_terminals.php
@@ -11,7 +11,7 @@ include_once(DIR_MODULES . "terminals/terminals.class.php");
 
 // ОБЯЗАТЕЛЬНО добавлять в список  если чего нового добавили
 $audio_terminals = array("mediaplayer", "mainterminal", "alicevox", "dnla_tts");
-$can_restored_audio = array("mediaplayer");
+$can_restored_audio = array("mediaplayer", "dnla_tts");
 
 // берем конфигурацию с модуля терминалов - общие настройки
 $ter = new terminals();
@@ -68,17 +68,20 @@ while (1) {
     
     $out_terminals = getObjectsByProperty('basy', '==', '0');
     foreach ($out_terminals as $terminals) {
+
         // если пустой терминал пропускаем
         if (!$terminals) {
             if ($ter->config['LOG_ENABLED']) DebMes("No free terminal " . $terminals, 'terminals');
             continue;
         }
         $terminal = SQLSelectOne("SELECT * FROM terminals WHERE LINKED_OBJECT = '" . $terminals . "'");
+ 
         // если пустая инфа о терминале пропускаем
         if (!$terminal) {
             if ($ter->config['LOG_ENABLED']) DebMes("No information of terminal" . $terminal['NAME'], 'terminals');
             continue;
         }
+
         // если терминалы офлайн то пингуем их
 		if (!$ter->config['TERMINALS_PING'] ) {
      		$ter->config['TERMINALS_PING'] = 10;
@@ -88,7 +91,8 @@ while (1) {
             pingTerminalSafe($terminal['NAME'], $terminal);
         }
         $old_message = SQLSelectOne("SELECT * FROM shouts WHERE ID <= '" . $number_message . "' AND SOURCE LIKE '%" . $terminal['ID'] . "^%' ORDER BY ID ASC");
-        // если отсутствует сообщение и тип плеер и есть инфа для восстановления то восстанавливаем воспроизводимое
+
+        // если отсутствует сообщение и есть тип плеер и есть инфа для восстановления то восстанавливаем воспроизводимое
         if (!$old_message['MESSAGE'] AND in_array($terminal['TTS_TYPE'],  $can_restored_audio )) {
             try {
                 $restored = gg($terminal['LINKED_OBJECT'] . '.playerdata');
@@ -108,10 +112,12 @@ while (1) {
                 if ($ter->config['LOG_ENABLED']) DebMes("Error with restore playaed - " . $terminal['NAME'], 'terminals');
             }
         }
+
         // для остальных плееров просто пропускаем итерацию и при отсутствии сообщения 
         if (!$old_message['MESSAGE']) {
             continue;
         }
+
         // если терминал оффлайн удаляем из работы эту запись и пропускаем (пингуется дополнительно - если вернется с ошибкой отправления)
         if (!$terminal['IS_ONLINE']) {
             $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
@@ -119,10 +125,12 @@ while (1) {
             if ($ter->config['LOG_ENABLED']) DebMes("Disable message - " . $terminal['NAME'], 'terminals');
             continue;
         }
+
         // если тип терминала воспроизводящий аудио и нету еще сгенерированного файла пропускаем 
         if (in_array($terminal['TTS_TYPE'], $audio_terminals) AND !$old_message['CACHED_FILENAME']) {
             continue;
         }
+
         // иначе запускаем его воспроизведение
         if (in_array($terminal['TTS_TYPE'], $audio_terminals) AND $old_message['CACHED_FILENAME']) {
             // убираем запись айди терминала из таблицы шутс - если не воспроизведется то вернет эту запись функция send_message($old_message, $terminal);
@@ -135,6 +143,7 @@ while (1) {
             if ($ter->config['LOG_ENABLED']) DebMes("Send message - " . $terminal['NAME'], 'terminals');
             continue;
         }
+
         // если тип терминала передающий только текстовое сообщение  
         // запускаем его воспроизведение
         if (!in_array($terminal['TTS_TYPE'], $audio_terminals)) {
