23a4f0b5ed917c410da38acf39256f6f19b9df28
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-26 15:19:28 +0300

    Update mainterminal.addon.php

--- modules/terminals/tts/mainterminal.addon.php
+++ modules/terminals/tts/mainterminal.addon.php
@@ -10,7 +10,7 @@ class mainterminal extends tts_addon
     }
 
     // Say
-    public function say_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка
+    public function say_media_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка
     {
         if($message['CACHED_FILENAME']) {
             if(file_exists($message['CACHED_FILENAME'])) {
