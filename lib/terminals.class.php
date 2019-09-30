4fd17e258bffec3793a6cc5f598e025a6d548bee
tarasfrompir <tarasfrompir@inbox.ru>
2019-09-30 23:21:44 +0300

    Update terminals.class.php

--- lib/terminals.class.php
+++ lib/terminals.class.php
@@ -364,13 +364,13 @@ function pingTerminal($terminal, $details)
         }
     }
     if ($out) {
-        sg($details['LINKED_OBJECT'] . '.status', '1');
+        sg($details['LINKED_OBJECT'] . '.alive', '1');
         $rec['LATEST_ACTIVITY'] = date('Y-m-d H:i:s');
         $rec['LATEST_REQUEST_TIME'] = date('Y-m-d H:i:s');
         $rec['IS_ONLINE'] = 1;
         DebMes("Terminal - " . $terminal .' is online', 'terminals');
     } else {
-        sg($details['LINKED_OBJECT'] . '.status', '0');
+        sg($details['LINKED_OBJECT'] . '.alive', '0');
         $rec['LATEST_REQUEST_TIME'] = date('Y-m-d H:i:s');
         $rec['IS_ONLINE'] = 0;
         DebMes("Terminal - " . $terminal .' is offline', 'terminals');
