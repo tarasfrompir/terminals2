dfa824a7de19fc8a8204ff1a1379cfdfc325b50e
tarasfrompir <tarasfrompir@inbox.ru>
2019-10-01 17:48:39 +0300

    Update alicevox.addon.php

--- modules/terminals/tts/alicevox.addon.php
+++ modules/terminals/tts/alicevox.addon.php
@@ -11,6 +11,7 @@ class alicevox extends tts_addon
         $this->setting = json_decode($this->terminal['TTS_SETING'], true);
         $this->title   = "Alicevox";
         $this->description = 'Работает на медиацентрах KODI  с установленным плагином  Alicevox. <a href="https://github.com/SergMicar/script.alicevox.master">Ссылка на плагин</a>  <a href="https://mjdm.ru/forum/viewtopic.php?f=5&t=2893&start=120">Ссылка на тему форума</a>' ;        
+        $this->description .= 'Пингование устройства проводится проверкой состояния сервиса.';
         $this->address = 'http://'.$this->setting['TTS_USERNAME'].':'.$this->setting['TTS_PASSWORD'].'@'.$this->terminal['HOST'].':'.(empty($this->setting['TTS_PORT'])?8080:$this->setting['TTS_PORT']);
         parent::__construct($terminal);
     }
