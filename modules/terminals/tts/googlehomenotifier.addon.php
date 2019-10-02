5b61f27ae8952f20b3c7b6399f9be365b9f31a5f
Sergey Goncharov <34092984+SergMicar@users.noreply.github.com>
2019-10-02 17:05:38 +0300

    правки дизайна описания

--- modules/terminals/tts/googlehomenotifier.addon.php
+++ modules/terminals/tts/googlehomenotifier.addon.php
@@ -7,9 +7,9 @@ class googlehomenotifier extends tts_addon {
         // содержит в себе все настройки терминала кроме айпи адреса
         $this->setting = json_decode($this->terminal['TTS_SETING'], true);
         $this->title="GoogleHomeNotifier";
-        $this->description = '<b>Описание:</b>&nbsp; Работает с google home устройствами через запущенный сервис &nbsp;<a href="https://github.com/noelportugal/google-home-notifier"google-home</a>. Передает текстовые сообщения с параметром языка, выбранного Вами в мажордомо<br>Ссылка на &nbsp;<a href="https://connect.smartliving.ru/profile/1502/blog38.html">how-to</a><br>Ссылка на &nbsp;<a href="https://mjdm.ru/forum/viewtopic.php?f=23&t=5042&hilit=google+home">тему форума</a><p>';
+        $this->description = '<b>Описание:</b>&nbsp; Работает с google home устройствами через запущенный сервис &nbsp;<a href="https://github.com/noelportugal/google-home-notifier" google-home</a>. Передает текстовые сообщения с параметром языка, выбранного Вами в мажордомо<br>Ссылка на &nbsp;<a href="https://connect.smartliving.ru/profile/1502/blog38.html">how-to</a><br>Ссылка на &nbsp;<a href="https://mjdm.ru/forum/viewtopic.php?f=23&t=5042&hilit=google+home">тему форума</a><p>';
         $this->description .= '<b>Поддерживаемые возможности:</b>&nbsp;say(), sayTo(), sayReply()<p>';
-        $this->description .= '<b>Проверка доступности:</b>&nbsp; ??? нужно разбираться ???';
+        $this->description .= '<b>Проверка доступности:</b>&nbsp; ??? нужно разбираться ???<p>';
         $this->description .= '<b>Настройка:</b>&nbsp; Порт доступа по умолчанию 8091';
         
         parent::__construct($terminal);
