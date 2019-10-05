3a95519f4229854a70155e968d219e494a421047
Sergey Goncharov <34092984+SergMicar@users.noreply.github.com>
2019-10-05 21:19:22 +0300

    В описании "Поддерживаемые возможности" теперь внизу

--- modules/terminals/tts/dnla_tts.addon.php
+++ modules/terminals/tts/dnla_tts.addon.php
@@ -10,9 +10,9 @@ class dnla_tts extends tts_addon
     {
         $this->title       = 'Устройства с поддержкой протокола DLNA';
 	$this->description = '<b>Описание:</b>&nbsp; Работает на всех устройства поддерживающих протокол DNLA.<br>';
-	$this->description .= '<b>Поддерживаемые возможности:</b>&nbsp;say(), sayTo(), sayReply().<br>';
 	$this->description .= '<b>Проверка доступности:</b>&nbsp;service_ping (пингование устройства проводится проверкой состояния сервиса).<br>';
-	$this->description .= '<b>Настройка:</b>&nbsp; Адрес управления вида http://ip:port/ (указывать не нужно, т.к. определяется автоматически и может отличаться для различных устройств).';
+	$this->description .= '<b>Настройка:</b>&nbsp; Адрес управления вида http://ip:port/ (указывать не нужно, т.к. определяется автоматически и может отличаться для различных устройств).<br>';
+	$this->description .= '<b>Поддерживаемые возможности:</b>&nbsp;say(), sayTo(), sayReply().';
         
 	$this->terminal = $terminal;
         $this->setting = json_decode($this->terminal['TTS_SETING'], true);
