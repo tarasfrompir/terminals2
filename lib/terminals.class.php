6fde64f64346f18d65df2d22aaa62a8498a6fa50
tarasfrompir <tarasfrompir@inbox.ru>
2019-10-10 08:45:59 +0300

    Update terminals.class.php

--- lib/terminals.class.php
+++ lib/terminals.class.php
@@ -1,6 +1,6 @@
 <?php
 // Get all terminals
-function getAllTerminals($limit = - 1, $order = 'ID', $sort = 'ASC') {
+function getAllTerminals($limit = -1, $order = 'ID', $sort = 'ASC') {
     $sqlQuery = 'SELECT * FROM `terminals` ORDER BY `' . DBSafe($order) . '` ' . DBSafe($sort);
     if ($limit >= 0) {
         $sqlQuery.= ' LIMIT ' . intval($limit);
@@ -17,7 +17,7 @@ function getTerminalByID($id) {
     return $terminal;
 }
 // Get terminal by name
-function getTerminalsByName($name, $limit = - 1, $order = 'ID', $sort = 'ASC') {
+function getTerminalsByName($name, $limit = -1, $order = 'ID', $sort = 'ASC') {
     $sqlQuery = "SELECT * FROM `terminals` WHERE `NAME` = '" . DBSafe($name) . "' OR `TITLE` = '" . DBSafe($name) . "' ORDER BY `" . DBSafe($order) . "` " . DBSafe($sort);
     if ($limit >= 0) {
         $sqlQuery.= ' LIMIT ' . intval($limit);
@@ -28,7 +28,7 @@ function getTerminalsByName($name, $limit = - 1, $order = 'ID', $sort = 'ASC') {
     return $terminals;
 }
 // Get terminals by host or ip address
-function getTerminalsByHost($host, $limit = - 1, $order = 'ID', $sort = 'ASC') {
+function getTerminalsByHost($host, $limit = -1, $order = 'ID', $sort = 'ASC') {
     $localhost = array('localhost', '127.0.0.1', 'ip6-localhost', 'ip6-loopback', 'ipv6-localhost', 'ipv6-loopback', '::1', '0:0:0:0:0:0:0:1',);
     if (in_array(strtolower($host), $localhost)) {
         $sqlQuery = "SELECT * FROM `terminals` WHERE `HOST` = '" . implode("' OR `HOST` = '", $localhost) . "' ORDER BY `" . DBSafe($order) . "` " . DBSafe($sort);
@@ -44,7 +44,7 @@ function getTerminalsByHost($host, $limit = - 1, $order = 'ID', $sort = 'ASC') {
     return $terminals;
 }
 // Get terminals that can play
-function getTerminalsCanPlay($limit = - 1, $order = 'ID', $sort = 'ASC') {
+function getTerminalsCanPlay($limit = -1, $order = 'ID', $sort = 'ASC') {
     $sqlQuery = "SELECT * FROM `terminals` WHERE `CANPLAY` = 1 ORDER BY `" . DBSafe($order) . "` " . DBSafe($sort);
     if ($limit >= 0) {
         $sqlQuery.= ' LIMIT ' . intval($limit);
@@ -55,7 +55,7 @@ function getTerminalsCanPlay($limit = - 1, $order = 'ID', $sort = 'ASC') {
     return $terminals;
 }
 // Get terminals by player type
-function getTerminalsByPlayer($player, $limit = - 1, $order = 'ID', $sort = 'ASC') {
+function getTerminalsByPlayer($player, $limit = -1, $order = 'ID', $sort = 'ASC') {
     $sqlQuery = "SELECT * FROM `terminals` WHERE `PLAYER_TYPE` = '" . DBSafe($player) . "' ORDER BY `" . DBSafe($order) . "` " . DBSafe($sort);
     if ($limit >= 0) {
         $sqlQuery.= ' LIMIT ' . intval($limit);
@@ -72,7 +72,7 @@ function getMainTerminal() {
     return $terminal;
 }
 // Get online terminals
-function getOnlineTerminals($limit = - 1, $order = 'ID', $sort = 'ASC') {
+function getOnlineTerminals($limit = -1, $order = 'ID', $sort = 'ASC') {
     $sqlQuery = "SELECT * FROM `terminals` WHERE `IS_ONLINE` = 1 ORDER BY `" . DBSafe($order) . "` " . DBSafe($sort);
     if ($limit >= 0) {
         $sqlQuery.= ' LIMIT ' . intval($limit);
@@ -83,7 +83,7 @@ function getOnlineTerminals($limit = - 1, $order = 'ID', $sort = 'ASC') {
     return $terminals;
 }
 // Get MajorDroid terminals
-function getMajorDroidTerminals($limit = - 1, $order = 'ID', $sort = 'ASC') {
+function getMajorDroidTerminals($limit = -1, $order = 'ID', $sort = 'ASC') {
     $sqlQuery = "SELECT * FROM `terminals` WHERE `MAJORDROID_API` = 1 ORDER BY `" . DBSafe($order) . "` " . DBSafe($sort);
     if ($limit >= 0) {
         $sqlQuery.= ' LIMIT ' . intval($limit);
