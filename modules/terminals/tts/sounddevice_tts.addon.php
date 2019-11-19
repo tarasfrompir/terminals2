44641bd55f448c3b4e71a22c39ba39701a0534c3
tarasfrompir <tarasfrompir@inbox.ru>
2019-11-19 19:32:59 +0200

    Update sounddevice_tts.addon.php

--- modules/terminals/tts/sounddevice_tts.addon.php
+++ modules/terminals/tts/sounddevice_tts.addon.php
@@ -4,33 +4,37 @@ class sounddevice_tts extends tts_addon
 {
     function __construct($terminal)
     {
-        $this->title = "Звуковые карты";
-	$this->description .= '<b>Работает:</b>&nbsp; только на Виндовс;.<br>';
-	$this->description .= '<b>Поддерживаемые возможности:</b>&nbsp;say(), sayTo(), sayReply().';    
+        $this->terminal = $terminal;
+        $this->title    = "Звуковые карты";
+        $this->description .= '<b>Работает:</b>&nbsp; только на Виндовс;.<br>';
+        $this->description .= '<b>Поддерживаемые возможности:</b>&nbsp;say(), sayTo(), sayReply().';
+        $this->setting = json_decode($this->terminal['TTS_SETING'], true);
         parent::__construct($terminal);
     }
-
+    
     // Say
     public function say_media_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка
     {
-        if($message['CACHED_FILENAME']) {
-		$fileinfo = pathinfo($message['CACHED_FILENAME']);
-		$filename = $fileinfo[dirname] . '/' . $fileinfo[filename] . '.wav';
-		if (!file_exists($filename)) {
-		    if (!defined('PATH_TO_FFMPEG')) {
-			if (IsWindowsOS()) {
-			    define("PATH_TO_FFMPEG", SERVER_ROOT . '/apps/ffmpeg/ffmpeg.exe');
-			} else {
-			    define("PATH_TO_FFMPEG", 'ffmpeg');
-			}
-		    }
-		    shell_exec(PATH_TO_FFMPEG . " -i " . $message['CACHED_FILENAME'] . " -acodec pcm_u8 -ar 44100 " . $fileinfo[dirname] . '/' . $fileinfo[filename] . '.wav');
-		 }
-            if(file_exists($message['CACHED_FILENAME'])) {
-                if (IsWindowsOS()){
-                    safe_exec(DOC_ROOT . '/rc/smallplayer.exe -play ' . $filename. ' '.$terminal['TTS_SOUND_DEVICE']);
-                } 
-                sleep ($message['MESSAGE_DURATION']);
+        if ($message['CACHED_FILENAME']) {
+            $fileinfo = pathinfo($message['CACHED_FILENAME']);
+            $filename = $fileinfo[dirname] . '/' . $fileinfo[filename] . '.wav';
+            if (!file_exists($filename)) {
+                if (!defined('PATH_TO_FFMPEG')) {
+                    if (IsWindowsOS()) {
+                        define("PATH_TO_FFMPEG", SERVER_ROOT . '/apps/ffmpeg/ffmpeg.exe');
+                    } else {
+                        define("PATH_TO_FFMPEG", 'ffmpeg');
+                    }
+                }
+                shell_exec(PATH_TO_FFMPEG . " -i " . $message['CACHED_FILENAME'] . " -acodec pcm_u8 -ar 22050 " . $fileinfo[dirname] . '/' . $fileinfo[filename] . '.wav');
+                DebMes($filename);
+            }
+            if (file_exists($message['CACHED_FILENAME'])) {
+                DebMes($this->setting['TTS_SOUND_DEVICE']);
+                if (IsWindowsOS()) {
+                    shell_exec(DOC_ROOT . '/rc/smallplayer.exe -play ' . $filename . ' ' . $this->setting['TTS_SOUND_DEVICE']);
+                }
+                sleep($message['MESSAGE_DURATION']);
                 $this->success = TRUE;
                 $this->message = 'OK';
             } else {
