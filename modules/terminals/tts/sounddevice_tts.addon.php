2e53ff16f371acf240696f694478e6316962e58b
tarasfrompir <tarasfrompir@inbox.ru>
2019-11-21 16:40:51 +0200

    Update sounddevice_tts.addon.php

--- modules/terminals/tts/sounddevice_tts.addon.php
+++ modules/terminals/tts/sounddevice_tts.addon.php
@@ -9,8 +9,8 @@ class sounddevice_tts extends tts_addon
         $this->description .= '<b>Работает:</b>&nbsp; только на Виндовс;.<br>';
         $this->description .= '<b>Поддерживаемые возможности:</b>&nbsp;say(), sayTo(), sayReply().';
         $this->setting = json_decode($this->terminal['TTS_SETING'], true);
-	$this->devicenumber = substr($this->setting['TTS_SOUND_DEVICE'], 0, strpos($this->setting['TTS_SOUND_DEVICE'], '^'));
-	$this->devicename = substr($this->setting['TTS_SOUND_DEVICE'], strpos($this->setting['TTS_SOUND_DEVICE'], '^')+1);
+        $this->devicenumber = substr($this->setting['TTS_SOUND_DEVICE'], 0, strpos($this->setting['TTS_SOUND_DEVICE'], '^'));
+        $this->devicename = substr($this->setting['TTS_SOUND_DEVICE'], strpos($this->setting['TTS_SOUND_DEVICE'], '^')+1);
         parent::__construct($terminal);
     }
     
@@ -48,5 +48,47 @@ class sounddevice_tts extends tts_addon
             $this->message = 'Input is missing!';
         }
         return $this->success;
+    }
+	
+	 // Set volume
+    function set_volume($level)
+    {
+        if (IsWindowsOS()) {
+            safe_exec(DOC_ROOT . '/rc/setvol.exe  ' . $level . ' device ' . $this->devicename);	
+        } else {
+             // linux
+        }
+
+        return TRUE;
+    }
+	
+	// Get player status
+    function status()
+    {
+        // Defaults
+        $track_id = -1;
+        $length   = 0;
+        $time     = 0;
+        $name     = 'unknow';
+        $state    = 'unknown';
+        $volume   = 0;
+        $random   = FALSE;
+        $loop     = FALSE;
+        $repeat   = FALSE;
+        
+        $this->data    = array(
+                'track_id' => (int) $result['status'][0]['media']['tracks'][0]['trackId'], //ID of currently playing track (in playlist). Integer. If unknown (playback stopped or playlist is empty) = -1.
+                'name' => (string) $name, //Current speed for playing media. float.
+                'file' => (string) $result['status'][0]['media']['contentId'], //Current link for media in device. String.
+                'length' => (int) $result['status'][0]['media']['duration'], //Track length in seconds. Integer. If unknown = 0. 
+                'time' => (int) $result['status'][0]['currentTime'], //Current playback progress (in seconds). If unknown = 0. 
+                'state' => (string) strtolower($result['status'][0]['playerState']), //Playback status. String: stopped/playing/paused/unknown 
+                'volume' => intval($status['status']['volume']['level']*100), // Volume level in percent. Integer. Some players may have values greater than 100.
+                'muted' => (int) $result['status'][0]['volume']['muted'], // Volume level in percent. Integer. Some players may have values greater than 100.
+                'random' => (boolean) $random, // Random mode. Boolean. 
+                'loop' => (boolean) $loop, // Loop mode. Boolean.
+                'repeat' => (string) $result['status'][0]['repeatMode'] //Repeat mode. Boolean.
+            );
+        return $this->data;
     }
 }
