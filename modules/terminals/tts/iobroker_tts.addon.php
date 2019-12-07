37452220a2028c4f13c5b5d191b88fe03d36def5
Aleks130699 <52662761+Aleks130699@users.noreply.github.com>
2019-12-07 17:34:51 +0700

    Create iobroker_tts.addon.php
    Добавление поддержки iobroker.paw

---
+++ modules/terminals/tts/iobroker_tts.addon.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+	Addon iobroker.paw http for app_player
+*/
+
+class iobroker_tts extends tts_addon {
+	
+
+    function __construct($terminal) {
+	$this->title="ioBroker.paw";  
+	$this->description = '<b>Поддерживаемые возможности:</b>say()<br>';
+	$this->description .= '<b>Описание:</b>&nbsp;Для работы использует &nbsp;<a href="https://play.google.com/store/apps/details?id=ru.codedevice.iobrokerpawii">ioBroker.paw</a>';
+	
+	$this->terminal = $terminal;
+	$this->curl = curl_init();
+	$this->address = 'http://' . $this->terminal['HOST'] . ':' . (empty($this->setting['TTS_PORT']) ? 8080 : $this->setting['TTS_PORT']);
+	parent::__construct($terminal);
+    }
+
+    // Say
+    function say_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка
+    {
+					$url = $this->address . "/api/set.json?ringtone=content%3A%2F%2Fmedia%2Finternal%2Faudio%2Fmedia%2F36";
+					getURLBackground($url,0);
+					sleep(1);
+					$url = $this->address . "/api/set.json?ringtone=false";
+					getURLBackground($url,0);
+					$url = $this->address . "/api/set.json?tts=" . urlencode($message['MESSAGE']);
+					getURLBackground($url,0);
+				}
+    }
+
+?>
