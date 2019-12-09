a911edd0e8aa4be33137440e1b8d50cf3ba43316
Aleks130699 <52662761+Aleks130699@users.noreply.github.com>
2019-12-09 19:27:37 +0700

    Update iobroker_tts.addon.php

--- modules/terminals/tts/iobroker_tts.addon.php
+++ modules/terminals/tts/iobroker_tts.addon.php
@@ -1,12 +1,9 @@
 <?php
-
 /*
 	Addon iobroker.paw http for app_player
 */
-
 class iobroker_tts extends tts_addon {
 	
-
     function __construct($terminal) {
 	$this->title="ioBroker.paw";  
 	$this->description = '<b>Поддерживаемые возможности:</b>say(),sayTo()<br>';
@@ -17,10 +14,9 @@ class iobroker_tts extends tts_addon {
         $this->port = empty($this->setting['TTS_PORT']) ? 8080 : $this->setting['TTS_PORT'];
 	$this->curl = curl_init();
 	$this->address = 'http://' . $this->terminal['HOST'] . ':' . $this->port;
-        $this->turnOnDisplay = $this->setting['TTS_USE_DISPLAY'];
+    $this->turnOnDisplay = $this->setting['TTS_USE_DISPLAY'];
 	$this->brightnes = $this->setting['TTS_BRIGHTNESS_DISPLAY'];
     }
-
     // Say
     function say_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка
     {
@@ -30,6 +26,7 @@ class iobroker_tts extends tts_addon {
 	usleep(500000);
 	$url = $this->address . "/api/set.json?tts=" . urlencode($message['MESSAGE']);
 	getURLBackground($url,0);
+	debMes($url);
     	sleep($message['MESSAGE_DURATION']);
         return true;
     }
@@ -38,14 +35,19 @@ class iobroker_tts extends tts_addon {
     {
 	if ($this->turnOnDisplay) {
 	    // включаем дисплей
+		getURLBackground($this->address . "/api/set.json?toWake=true",0);
+		debMes($this->address . "/api/set.json?toWake=true");
+		usleep(500000);
 	}
         return true;
     }
-
     function turn_off_display($terminal) 
     {
 	if ($this->turnOnDisplay) {
 	    // выключаем дисплей
+		getURLBackground($this->address . "/api/set.json?toWake=false",0);
+		usleep(100000);
+		debMes($this->address . "/api/set.json?toWake=false");
 	}  
         return true;
     }
@@ -54,10 +56,13 @@ class iobroker_tts extends tts_addon {
     {
     	if ($this->turnOnDisplay) {
              // установим яркость дисплея
+			$url = $this->address . "/api/set.json?brightness=" . $this->brightnes;
+			getURLBackground($url,0);
+			debMes($url);
+			usleep(500000);
 	}
         return true;
     }
 	
 }
-
 ?>
