d11157e482c20a8fba99cedc3cfcdb6ee0b5437e
tarasfrompir <tarasfrompir@inbox.ru>
2019-12-11 17:51:09 +0200

    Update sounddevice_tts.addon.php

--- modules/terminals/tts/sounddevice_tts.addon.php
+++ modules/terminals/tts/sounddevice_tts.addon.php
@@ -36,7 +36,7 @@ class sounddevice_tts extends tts_addon
                 } else {
                     // linux
                 }
-                sleep($message['MESSAGE_DURATION']);
+                sleep($message['MESSAGE_DURATION']+1);
                 $this->success = TRUE;
                 $this->message = 'OK';
             } else {
