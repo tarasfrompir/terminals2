701dd4825d13b286232b47966a917dea5c2156f2
tarasfrompir <tarasfrompir@inbox.ru>
2019-12-12 10:42:52 +0200

    Update mainterminal.addon.php

--- modules/terminals/tts/mainterminal.addon.php
+++ modules/terminals/tts/mainterminal.addon.php
@@ -35,4 +35,40 @@ class mainterminal extends tts_addon
         }
         return $this->success;
     }
+	  // Get player status
+    function status()
+    {
+        // Defaults
+        $track_id = -1;
+        $length   = '';
+        $time     = '';
+        $state    = 'unknown';
+        $volume   = '';
+        $muted    = FALSE;
+        $random   = FALSE;
+        $loop     = FALSE;
+        $repeat   = FALSE;
+        $name     = 'unknow';
+        $file     = '';
+        $brightness = '';
+        $display_state = 'unknown';
+        
+        $this->data = array(
+                'track_id' => $track_id, //ID of currently playing track (in playlist). Integer. If unknown (playback stopped or playlist is empty) = -1.
+                'name' => $name, //Current speed for playing media. float.
+                'file' => $file, //Current link for media in device. String.
+                'length' => $length, //Track length in seconds. Integer. If unknown = 0. 
+                'time' => $time, //Current playback progress (in seconds). If unknown = 0. 
+                'state' => strtolower($state), //Playback status. String: stopped/playing/paused/unknown 
+                'volume' => $volume, // Volume level in percent. Integer. Some players may have values greater than 100.
+                'muted' => $muted, // Muted mode. Boolean.
+                'random' => $random, // Random mode. Boolean. 
+                'loop' => $loop, // Loop mode. Boolean.
+                'repeat' => $repeat, //Repeat mode. Boolean.
+                'brightness'=> $brightness, // brightness display in %
+                'display_state'=> $display_state, // unknow , On, Off  - display  state
+                
+            );
+        return $this->data;
+    }
 }
