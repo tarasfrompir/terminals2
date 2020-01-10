32f1d49915e34ecf9e6409efc77ce028263c324d
tarasfrompir <tarasfrompir@inbox.ru>
2020-01-10 16:49:00 +0200

    Add files via upload

--- modules/terminals/tts/airplay_tts.addon.php
+++ modules/terminals/tts/airplay_tts.addon.php
@@ -12,8 +12,7 @@ class airplay_tts extends tts_addon
         $this->terminal      = $terminal;
         $this->setting       = json_decode($this->terminal['TTS_SETING'], true);
         $this->port          = empty($this->setting['TTS_PORT']) ? 7000 : $this->setting['TTS_PORT'];
-        $this->address       = 'http://' . $this->terminal['HOST'] . ':' . $this->port;
-		include_once(DIR_MODULES . 'app_player/libs/Airplay/airplay.php');
+        include_once(DIR_MODULES . 'app_player/libs/Airplay/airplay.php');
     }
 
     // Say
