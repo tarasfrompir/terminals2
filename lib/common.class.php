4b03909e1c4f4a2ef3cfa5012b19a8c5b1db4c1f
tarasfrompir <tarasfrompir@inbox.ru>
2020-01-14 16:36:53 +0200

    Update common.class.php

--- lib/common.class.php
+++ lib/common.class.php
@@ -399,7 +399,7 @@ function say($ph, $level = 0, $member_id = 0, $source = '')
     
     // костыль для яндекс станций - переделать когда устаткуют все 
     if (file_exists(DIR_MODULES . "yadevices/yadevices.class.php")) {
-        include_once (DIR_MODULES . "yadevices/yadevices.class.php"));
+        include_once (DIR_MODULES . "yadevices/yadevices.class.php");
         $yadevice = new yadevices();
 	//$yadevice->processSubscription('SAY', array('level' => $level, 'message' => $ph, ));
         $stations = SQLSelect("SELECT ID FROM yastations WHERE TTS=1 AND MIN_LEVEL<=".(int)$level);
