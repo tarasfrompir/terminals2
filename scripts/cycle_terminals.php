de24f334cabc35e72287e791b626ca50a7aed478
tarasfrompir <tarasfrompir@inbox.ru>
2020-01-24 11:15:59 +0200

    Update cycle_terminals.php

--- scripts/cycle_terminals.php
+++ scripts/cycle_terminals.php
@@ -98,6 +98,8 @@ while (1) {
             try {
                 if ($ter->config['LOG_ENABLED'])
                     DebMes("PingSafe terminal " . $terminal['NAME'], 'terminals');
+				//установим флаг занятости терминала 
+				sg($terminal['LINKED_OBJECT'] . '.busy', 1);
                 pingTerminalSafe($terminal['NAME'], $terminal);
                 continue;
             }
