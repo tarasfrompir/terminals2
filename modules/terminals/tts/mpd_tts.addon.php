59f03c3a6a32d4586ad0e91573217e045516faf6
tarasfrompir <tarasfrompir@inbox.ru>
2020-01-29 11:05:46 +0200

    Update mpd_tts.addon.php

--- modules/terminals/tts/mpd_tts.addon.php
+++ modules/terminals/tts/mpd_tts.addon.php
@@ -1,42 +1,31 @@
 <?php
 
 /*
-	Addon MPD for app_player
+Addon MPD for tts
 */
 
 class mpd_tts extends tts_addon {
-
-	// Private properties
-	private $mpd;
-
-	// Constructor
-	function __construct($terminal) {
-            $this->title = 'Music Player Daemon (MPD)';
-            $this->description = '<b>Описание:</b>&nbsp; Воспроизведение звука через кроссплатформенный музыкальный проигрыватель, который имеет клиент-серверную архитектуру.<br>';
-
-            // Network
-            $this->terminal = $terminal;
-            $this->setting  = json_decode($this->terminal['TTS_SETING'], true);
-            $this->port     = empty($this->setting['TTS_PORT']) ? 6600 : $this->setting['TTS_PORT'];
-            $this->password = $this->setting['TTS_PASSWORD'];
-		// MPD
-		include_once(DIR_MODULES.'app_player/libs/mpd/mpd.class.php');
-	}
-
-	// Private: MPD connect
-	private function mpd_connect() {
-		$this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
-		if($this->mpd->connected) {
-			$this->success = TRUE;
-		} else {
-			$this->success = FALSE;
-		}
-		return $this->success;
-	}
-	
-	// Say
-    function say_media_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка
+    
+    // Private properties
+    private $mpd;
+    
+    // Constructor
+    function __construct($terminal)
     {
+        $this->title       = 'Music Player Daemon (MPD)';
+        $this->description = '<b>Описание:</b>&nbsp; Воспроизведение звука через кроссплатформенный музыкальный проигрыватель, который имеет клиент-серверную архитектуру.<br>';
+        $this->terminal    = $terminal;
+        $this->setting     = json_decode($this->terminal['TTS_SETING'], true);
+        $this->port        = empty($this->setting['TTS_PORT']) ? 6600 : $this->setting['TTS_PORT'];
+        $this->password    = $this->setting['TTS_PASSWORD'];
+        DebMes($this->terminal['HOST'] . $this->port . $this->password);
+        // MPD
+        include_once(DIR_MODULES . 'app_player/libs/mpd/mpd.class.php');
+        
+    }
+    
+    // Say
+    function say_media_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка {
         $outlink = $message['CACHED_FILENAME'];
         // берем ссылку http
         if (preg_match('/\/cms\/cached.+/', $outlink, $m)) {
@@ -48,69 +37,95 @@ class mpd_tts extends tts_addon {
                 $message_link = 'http://' . $server_ip . $m[0];
             }
         }
-        
-        $this->mpd_connect();
-		$this->mpd->PLClear();
-		$this->mpd->PLAdd($message_link);
-		$this->mpd->Play();
-		$this->mpd->Disconnect();
-		sleep($message['MESSAGE_DURATION']);
-        $this->success = TRUE;
+        $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
+        if ($this->mpd->connected) {
+            $this->mpd->PLClear();
+            $this->mpd->PLAdd($message_link);
+            if ($this->mpd->Play()) {
+                sleep($message['MESSAGE_DURATION']);
+                $this->success = TRUE;
+            } else {
+                $this->success = FALSE;
+            }
+        } else {
+            $this->success = FALSE;
+        }
+        $this->mpd->Disconnect();
         return $this->success;
     }
-	
-	// Play
-	function play($input, $time = 0) {
-		if(strlen($input)) {
-			if($this->mpd_connect()) {
-				$this->mpd->PLClear();
-				$this->mpd->PLAdd($input);
-				$this->mpd->Play();
-				$this->mpd->Disconnect();
-				$this->success = TRUE;
-			}
-		} else {
-			$this->success = FALSE;
-		}
-		return $this->success;
-	}
-
-	// Stop
-	function stop() {
-		if($this->mpd_connect()) {
-			$this->mpd->Stop();
-			$this->mpd->Disconnect();
-			$this->reset_properties();
-			$this->success = TRUE;
-		}
-		return $this->success;
-	}
-
-	// Set volume
-	function set_volume($level) {
-		if(strlen($level)) {
-			if($this->mpd_connect()) {
-				$this->mpd->SetVolume((int)$level);
-				$this->mpd->Disconnect();
-				$this->reset_properties();
-				$this->success = TRUE;
-			}
-		} else {
-			$this->success = FALSE;
-		}
-		return $this->success;
-	}
+    
+    // Play
+    function play($input, $time = 0) {
+        if (strlen($input)) {
+            $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
+            if ($this->mpd->connected) {
+                $this->mpd->PLClear();
+                $this->mpd->PLAdd($input);
+                if ($this->mpd->Play()) {
+                    
+                    $this->success = TRUE;
+                } else {
+                    $this->success = FALSE;
+                }
+            } else {
+                $this->success = FALSE;
+            }
+            $this->mpd->Disconnect();
+        } else {
+            $this->success = FALSE;
+        }
+        return $this->success;
+    }
+    // Stop
+    function stop() {
+        $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
+        if ($this->mpd->connected) {
+            $this->mpd->PLClear();
+            $this->mpd->PLAdd($input);
+            if ($this->mpd->Stop()) {
+                $this->success = TRUE;
+            } else {
+                $this->success = FALSE;
+            }
+        } else {
+            $this->success = FALSE;
+        }
+        $this->mpd->Disconnect();
+        return $this->success;
+    }
+    
+    // Set volume
+    function set_volume($level) {
+        if (strlen($level)) {
+            $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
+            if ($this->mpd->connected) {
+                $this->mpd->PLClear();
+                $this->mpd->PLAdd($input);
+                if (SetVolume((int) $level)) {
+                    $this->success = TRUE;
+                } else {
+                    $this->success = FALSE;
+                }
+            } else {
+                $this->success = FALSE;
+            }
+            $this->mpd->Disconnect();
+        } else {
+            $this->success = FALSE;
+        }
+        return $this->success;
+    }
     // ping terminal
-    function ping()
-    {
-        if ($this->mpd_connect()) {
+    function ping() {
+        $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
+        if ($this->mpd->connected) {
             $this->success = TRUE;
         } else {
             $this->success = FALSE;
         }
         return $this->success;
     }
-
+    
 }
 
 ?>
