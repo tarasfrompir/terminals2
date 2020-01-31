48144a25b1257622bbbea1fa6216885bc3b9f8fb
tarasfrompir <tarasfrompir@inbox.ru>
2020-01-31 14:06:58 +0200

    Update mpd_tts.addon.php

--- modules/terminals/tts/mpd_tts.addon.php
+++ modules/terminals/tts/mpd_tts.addon.php
@@ -5,10 +5,7 @@ Addon MPD for tts
 */
 
 class mpd_tts extends tts_addon {
-    
-    // Private properties
-    private $mpd;
-    
+
     // Constructor
     function __construct($terminal) {
         $this->title       = 'Music Player Daemon (MPD)';
@@ -18,98 +15,60 @@ class mpd_tts extends tts_addon {
         $this->port        = empty($this->setting['TTS_PORT']) ? 6600 : $this->setting['TTS_PORT'];
         $this->password    = $this->setting['TTS_PASSWORD'];
         // MPD
-        include_once (DIR_MODULES . 'app_player/libs/mpd/mpd.class.php');
-        
+        include (DIR_MODULES . 'app_player/libs/mpd/mpd.class.php');
+        $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);        
     }
     
     // Say
     function say_media_message($message, $terminal) {
-        $outlink = $message['CACHED_FILENAME'];
-        // берем ссылку http
-        if (preg_match('/\/cms\/cached.+/', $outlink, $m)) {
-            $server_ip = getLocalIp();
-            if (!$server_ip) {
-                DebMes("Server IP not found", 'terminals');
-                return false;
-            } else {
-                $message_link = 'http://' . $server_ip . $m[0];
+        if ($this->mpd->connected) {
+            $outlink = $message['CACHED_FILENAME'];
+            // берем ссылку http
+            if (preg_match('/\/cms\/cached.+/', $outlink, $m)) {
+                $server_ip = getLocalIp();
+                if (!$server_ip) {
+                    DebMes("Server IP not found", 'terminals');
+                    return false;
+                } else {
+                    $message_link = 'http://' . $server_ip . $m[0];
+                }
             }
-        }
-        $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
-        if (!is_null($this->mpd->connected)) {
+            //DebMes($this->mpd->SetVolume(50));
+            DebMes($this->mpd->GetStatus());
+            //DebMes($this->mpd->Stop());
+            //DebMes($this->mpd->GetStatus());
+            //DebMes($this->mpd->Play());
             $this->mpd->PLClear();
             $this->mpd->PLAdd($message_link);
-            $this->mpd->Play();
-            sleep($message['MESSAGE_DURATION']);
-            $this->success = TRUE;
+            if ($this->mpd->Play()) {
+                sleep($message['MESSAGE_DURATION']);
+                $this->mpd->Stop();
+                $this->success = TRUE;                
+            } else {
+                $this->success = FALSE;
+            }
         } else {
             $this->success = FALSE;
         }
         $this->mpd->Disconnect();
         return $this->success;
     }
-    
-    // Play
-    function play($input, $time = 0) {
-        if (strlen($input)) {
-            $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
-            if (!is_null($this->mpd->connected)) {
-                $this->mpd->PLClear();
-                $this->mpd->PLAdd($input);
-                $this->mpd->SeekTo($time);
-                $this->mpd->Play();
-                $this->success = TRUE;
-            } else {
-                $this->success = FALSE;
-            }
-            $this->mpd->Disconnect();
-        } else {
-            $this->success = FALSE;
-        }
-        return $this->success;
-    }
-    // Stop
-    function stop() {
-        $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
-        if (!is_null($this->mpd->connected)) {
-            $this->mpd->Stop();
-            $this->success = TRUE;
-        } else {
-            $this->success = FALSE;
-        }
-        $this->mpd->Disconnect();
-        return $this->success;
-    }
-    
-    // Set volume
-    function set_volume($level) {
-        if (strlen($level)) {
-            $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
-            if (!is_null($this->mpd->connected)) {
-                $this->mpd->SetVolume((int) $level);
-                $this->success = TRUE;
-            } else {
-                $this->success = FALSE;
-            }
-            $this->mpd->Disconnect();
-        } else {
-            $this->success = FALSE;
-        }
-        return $this->success;
-    }
-    
+
+
     // ping terminal
     function ping() {
-        $this->mpd = new mpd_player($this->terminal['HOST'], $this->port, $this->password);
-        if (!is_null($this->mpd->connected)) {
-            $this->success = TRUE;
+        if ($this->mpd->connected) {
+            if ($this->mpd->Ping()) {
+            $this->success = TRUE;    
+            } else {
+                $this->success = FALSE;
+            }
         } else {
             $this->success = FALSE;
         }
         $this->mpd->Disconnect();
         return $this->success;
     }
-    
 }
 
 ?>
