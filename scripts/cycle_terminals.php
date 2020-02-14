765f2dee7359590270426a5af9e03554b64a5756
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-14 12:35:56 +0200

    Update cycle_terminals.php

--- scripts/cycle_terminals.php
+++ scripts/cycle_terminals.php
@@ -16,7 +16,7 @@ $checked_time = 0;
 // set all terminal as free when restart cycle
 $term         = SQLSelect("SELECT * FROM terminals");
 foreach ($term as $t) {
-    sg($t['LINKED_OBJECT'] . '.busy', 0);
+    sg($t['LINKED_OBJECT'] . '.TerminalState', 0);
 }
 // reset all message when reload cicle
 //SQLExec("UPDATE shouts SET SOURCE = '' ");
@@ -68,7 +68,7 @@ while (1) {
         sleep(1);
     }
     
-    $out_terminals = getObjectsByProperty('busy', '==', '0');
+    $out_terminals = getObjectsByProperty('TerminalState', '==', '0');
     foreach ($out_terminals as $terminals) {
         // если нету свободных терминалов пропускаем
         if (!$terminals) {
