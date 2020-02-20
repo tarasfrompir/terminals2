dbf36ce8d737885c19893b5b0115b118ba12afde
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-20 13:26:50 +0200

    Update mpd.addon.php

--- modules/app_player/addons/mpd.addon.php
+++ modules/app_player/addons/mpd.addon.php
@@ -89,8 +89,7 @@ class mpd extends app_player_addon {
                 'crossfade' => (int) $crossfade, // crossfade
                 'speed' => (int) $speed, // crossfade
             );
-        }
-		// удаляем из массива пустые данные
+ 		// удаляем из массива пустые данные
 		foreach ($this->data as $key => $value) {
 			if ($value == '-1' or !$value) unset($this->data[$key]);
 		}
