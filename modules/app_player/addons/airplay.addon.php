34033425d5a0e31bb80176a12e06dd121d5e2368
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-20 13:14:45 +0200

    Update airplay.addon.php

--- modules/app_player/addons/airplay.addon.php
+++ modules/app_player/addons/airplay.addon.php
@@ -60,7 +60,6 @@ class airplay extends app_player_addon
                 'crossfade' => (int) $crossfade, // crossfade
                 'speed' => (int) $speed, // crossfade
             );
-        }
 		// удаляем из массива пустые данные
 		foreach ($this->data as $key => $value) {
 			if ($value == '-1' or !$value) unset($this->data[$key]);
