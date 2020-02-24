b929f65185a7d5c4f55db73dfb9d485f9036f932
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-24 16:28:36 +0200

    Update majordroid_tts.addon.php

--- modules/terminals/tts/majordroid_tts.addon.php
+++ modules/terminals/tts/majordroid_tts.addon.php
@@ -5,16 +5,16 @@ class majordroid_tts extends tts_addon
     
     function __construct($terminal)
     {
-        parent::__construct($terminal);
-        if (!$this->terminal['HOST']) return false;
-
         $this->title       = "MajorDroid";
         $this->setting     = json_decode($this->terminal['TTS_SETING'], true);
         $this->description = '<b>Описание:</b>&nbsp; Используется на устройствах которые поддерживаают MajorDroid API.<br>';
         $this->description .= '<b>Проверка доступности:</b>&nbsp;service_ping (пингование устройства проводится проверкой состояния сервиса).<br>';
         $this->description .= '<b>Настройка:</b>&nbsp; Порт доступа по умолчанию 7999 (если по умолчанию, можно не указывать).<br>';
         $this->description .= '<b>Поддерживаемые возможности:</b>&nbsp;say(), sayTo(), sayReply(), ask().';
-        $this->port = empty($this->setting['TTS_PORT']) ? 7999 : $this->setting['TTS_PORT'];
+
+	            parent::__construct($terminal);
+        if (!$this->terminal['HOST']) return false;
+	    $this->port = empty($this->setting['TTS_PORT']) ? 7999 : $this->setting['TTS_PORT'];
         register_shutdown_function("catchTimeoutTerminals");
     }
     
