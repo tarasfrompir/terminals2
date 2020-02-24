2e9c985710a35e61ebbeaf15905aaea4c333c100
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-24 20:19:26 +0200

    Update cycle_terminals.php

--- scripts/cycle_terminals.php
+++ scripts/cycle_terminals.php
@@ -8,7 +8,7 @@ include_once("./load_settings.php");
 include_once(DIR_MODULES . "terminals/terminals.class.php");
 include_once DIR_MODULES . 'terminals/tts_addon.class.php';
 // обьявляем массив обектов дабы не грузить их всегда 
-$tts = array();
+$base_terminal = array();
 // берем конфигурацию с модуля терминалов - общие настройки
 $ter = new terminals();
 $ter->getConfig();
@@ -101,10 +101,10 @@ while (1) {
             continue;
         }
        
-        // обьявляем новый обьект которого нет в массиве $tts                
-        if (!$tts[$terminal['ID']] AND $terminal['TTS_TYPE'] AND file_exists(DIR_MODULES . 'terminals/tts/' . $terminal['TTS_TYPE'] . '.addon.php')) {
+        // обьявляем новый обьект которого нет в массиве $base_terminal                
+        if (!$base_terminal[$terminal['ID']] AND $terminal['TTS_TYPE'] AND file_exists(DIR_MODULES . 'terminals/tts/' . $terminal['TTS_TYPE'] . '.addon.php')) {
             include_once(DIR_MODULES . 'terminals/tts/' . $terminal['TTS_TYPE'] . '.addon.php');
-            $tts[$terminal['ID']] = new $terminal['TTS_TYPE']($terminal);
+            $base_terminal[$terminal['ID']] = new $terminal['TTS_TYPE']($terminal);
             if ($ter->config['LOG_ENABLED']) DebMes("Add terminal to array tts objects -" . $terminal['NAME'], 'terminals');
             continue;
         }
@@ -167,7 +167,7 @@ while (1) {
         
         // если есть сообщение НО не сгенерирован звук (остутсвует в информации о сообщении запись) в течении 2 минут 
         // удаляем сообщение из очереди для терминалов воспроизводящих звук
-        if ($old_message['CACHED_FILENAME'] AND strtotime($old_message['ADDED']) + 2 * 60 < time() AND method_exists($tts[$terminal['ID']], 'say_media_message')) {
+        if ($old_message['CACHED_FILENAME'] AND strtotime($old_message['ADDED']) + 2 * 60 < time() AND method_exists($base_terminal[$terminal['ID']], 'say_media_message')) {
             try {
                 $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
                 SQLUpdate('shouts', $old_message);
@@ -187,7 +187,7 @@ while (1) {
         
         // если есть сообщение и есть запись о существовании файла НО не сгенерирован звук (отсутсвтует файл)
         // удаляем сообщение из очереди для терминалов воспроизводящих звук
-        if ($old_message['CACHED_FILENAME'] AND !file_exists($old_message['CACHED_FILENAME']) AND method_exists($tts[$terminal['ID']], 'say_media_message')) {
+        if ($old_message['CACHED_FILENAME'] AND !file_exists($old_message['CACHED_FILENAME']) AND method_exists($base_terminal[$terminal['ID']], 'say_media_message')) {
             try {
                 $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
                 SQLUpdate('shouts', $old_message);
@@ -205,9 +205,14 @@ while (1) {
             }
         }
 
+        // если тип терминала воспроизводящий аудио и нету еще сгенерированного файла пропускаем
+        if (method_exists($base_terminal[$terminal['ID']], 'say_media_message') AND !$old_message['CACHED_FILENAME']) {
+            continue;
+        }
+
         // если тип терминала передающий только текстовое сообщение  
         // запускаем его воспроизведение
-        if (method_exists($tts[$terminal['ID']], 'say_message') AND $old_message['SOURCE']) {
+        if (method_exists($base_terminal[$terminal['ID']], 'say_message') AND $old_message['SOURCE']) {
             try {
                 // убираем запись айди терминала из таблицы шутс - если не воспроизведется то вернет эту запись функция send_message($old_message, $terminal);
                 $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
@@ -224,14 +229,9 @@ while (1) {
             }
         }
 	    
-        // если тип терминала воспроизводящий аудио и нету еще сгенерированного файла пропускаем
-        if (method_exists($tts[$terminal['ID']], 'say_media_message') AND !$old_message['CACHED_FILENAME']) {
-            continue;
-        }
-        
         // если тип терминала передающий медиа сообщение
         // иначе запускаем его воспроизведение
-        if (method_exists($tts[$terminal['ID']], 'say_media_message') AND $old_message['CACHED_FILENAME'] AND $old_message['SOURCE']) {
+        if (method_exists($base_terminal[$terminal['ID']], 'say_media_message') AND $old_message['CACHED_FILENAME'] AND $old_message['SOURCE']) {
             try {
                 // убираем запись айди терминала из таблицы шутс - если не воспроизведется то вернет эту запись функция send_message($old_message, $terminal);
                 $old_message['SOURCE'] = str_replace($terminal['ID'] . '^', '', $old_message['SOURCE']);
