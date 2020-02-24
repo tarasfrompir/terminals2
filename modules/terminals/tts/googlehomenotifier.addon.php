90d39b60baad4c448822482e4f90d2fb1b60fd51
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-24 16:30:07 +0200

    Update googlehomenotifier.addon.php

--- modules/terminals/tts/googlehomenotifier.addon.php
+++ modules/terminals/tts/googlehomenotifier.addon.php
@@ -3,17 +3,18 @@
 class googlehomenotifier extends tts_addon {
 
     function __construct($terminal) {
-        parent::__construct($terminal);
-        if (!$this->terminal['HOST']) return false;
-	    
-        // содержит в себе все настройки терминала кроме айпи адреса
-        $this->setting = json_decode($this->terminal['TTS_SETING'], true);
         $this->title="GoogleHomeNotifier";
         $this->description = '<b>Описание:</b>&nbsp; Работает с google home устройствами через запущенный сервис &nbsp;<a href="https://github.com/noelportugal/google-home-notifier">google-home</a>. Передает текстовые сообщения с параметром языка, выбранного Вами в мажордомо.<br>Ссылка на &nbsp;<a href="https://connect.smartliving.ru/profile/1502/blog38.html">how-to</a>.<br>Ссылка на &nbsp;<a href="https://mjdm.ru/forum/viewtopic.php?f=23&t=5042&hilit=google+home">тему форума</a>.<br>';
         $this->description .= '<b>Проверка доступности:</b>&nbsp; ??? нужно разбираться ???.<br>';
         $this->description .= '<b>Настройка:</b>&nbsp; Порт доступа по умолчанию 8091 (если по умолчанию, можно не указывать).<br>';
         $this->description .= '<b>Поддерживаемые возможности:</b>&nbsp;say(), sayTo(), sayReply().';
-        register_shutdown_function("catchTimeoutTerminals");
+        parent::__construct($terminal);
+        if (!$this->terminal['HOST']) return false;
+	    
+        // содержит в себе все настройки терминала кроме айпи адреса
+        $this->setting = json_decode($this->terminal['TTS_SETING'], true);
+	    
+	    register_shutdown_function("catchTimeoutTerminals");
     }
 
     // Say
