2a71aa410e10951b61546e526de121a6addda1db
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-24 15:14:43 +0200

    Update mjdmterminal_tts.addon.php

--- modules/terminals/tts/mjdmterminal_tts.addon.php
+++ modules/terminals/tts/mjdmterminal_tts.addon.php
@@ -20,12 +20,7 @@ class mjdmterminal_tts extends tts_addon
     {
         return $this->sendMjdmCommand('tts:' . $message['MESSAGE']);
     }
-    
-    function play($input, $time = 0) //SETTINGS_SITE_LANGUAGE_CODE=код языка
-    {
-        return $this->sendMjdmCommand('play:' . $input);
-    }
-    
+
     function ask($phrase, $level = 0)
     {
         return $this->sendMjdmCommand('ask:' . $phrase);
@@ -46,57 +41,7 @@ class mjdmterminal_tts extends tts_addon
         // установим яркость дисплея
         return $this->sendMjdmCommand('brightness:' . $brightness);
     }
-    
-    // Get player status
-    function status()
-    {
-        // Defaults
-        $playlistid    = -1;
-        $playlist_content = '';
-        $track_id      = -1;
-        $length        = 0;
-        $time          = 0;
-        $file          = '';
-        $name          = 'unknow';
-        $state         = 'unknown';
-        $volume        = 0;
-        $random        = FALSE;
-        $loop          = FALSE;
-        $repeat        = FALSE;
-        $brightness    = '';
-        $display_state = false;
-        $battery = false;
-        $result   = json_decode($this->sendMjdmCommand('status'));
-        
-       if ($result) {
-            $this->data = array(
-                'playlist_id' => $playlistid, // номер или имя плейлиста 
-                'playlist_content' => $playlist_content, // содержимое плейлиста должен быть ВСЕГДА МАССИВ 
-                                                         // обязательно $playlist_content[$i]['pos'] - номер трека
-                                                         // обязательно $playlist_content[$i]['file'] - адрес трека
-                                                         // возможно $playlist_content[$i]['Artist'] - артист
-                                                         // возможно $playlist_content[$i]['Title'] - название трека
-                'track_id' => (int) $track_id, //текущий номер трека
-                'name' => (string) $name, //текущее имя трека 
-                'file' => (string) $file, //ссылка на текущий файл .
-                'length' => intval($result['duration']), //длинна плейлиста (песни). 
-                'time' => intval($result['time']), //текущая позиции по времени трека 
-                'state' => (string) strtolower($result['state']), //Playback status. String: stopped/playing/paused/unknown 
-                'volume' => (int)rtrim($result['volume_media'],'%'), // Volume level in percent. Integer. Some players may have values greater than 100.
-                'muted' => intval($result['muted']), // Volume level in percent. Integer. Some players may have values greater than 100.
-                'random' => (boolean) $result['random'], // Random mode. Boolean. 
-                'loop' => (boolean) $result['loop'], // Loop mode. Boolean.
-                'crossfade' => (boolean) $result['xfade'], // crossfade
-                'repeat' => (boolean) $result['repeat'], //Repeat mode. Boolean.
-                'brightness' => (int) rtrim($result['brightness'],'%'), // яркость екрана
-		'battery' => (int) rtrim($result['battery'],'%'), // заряд терминала екрана
-                'display_state' => (boolean) ($display_state) // состояние екрана включен (выключен)
-            );
-        }
-        return $this->data;
-    }
-    
-	// Get terminal status
+    // Get terminal status
     function terminal_status()
     {
         // Defaults
@@ -134,7 +79,7 @@ class mjdmterminal_tts extends tts_addon
         return $out_data;
     }
 	
-    function sendMjdmCommand($cmd)
+    private function sendMjdmCommand($cmd)
     {
         if ($this->terminal['HOST']) {
             $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
