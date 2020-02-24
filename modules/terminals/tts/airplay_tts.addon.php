688cf53c2a3397c06e70ae6939107bc7fbea0177
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-24 16:11:02 +0200

    Update airplay_tts.addon.php

--- modules/terminals/tts/airplay_tts.addon.php
+++ modules/terminals/tts/airplay_tts.addon.php
@@ -8,9 +8,9 @@ class airplay_tts extends tts_addon
     function __construct($terminal)
     {
         parent::__construct($terminal);
+        if (!$this->terminal['HOST']) return false;
         $this->title       = "Airplay";
         $this->description = '<b>Поддерживаемые возможности:</b>say(),sayTo()<br>';
-	if (!$this->terminal['HOST']) return false;
         $this->setting     = json_decode($this->terminal['TTS_SETING'], true);
         $this->port        = empty($this->setting['TTS_PORT']) ? 7000 : $this->setting['TTS_PORT'];
         include_once(DIR_MODULES . 'app_player/libs/Airplay/airplay.php');
