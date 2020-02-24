1942f9a3eda58726813868a1badc2b7aa07a34e5
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-24 16:26:47 +0200

    Update sounddevice_tts.addon.php

--- modules/terminals/tts/sounddevice_tts.addon.php
+++ modules/terminals/tts/sounddevice_tts.addon.php
@@ -4,13 +4,13 @@ class sounddevice_tts extends tts_addon
 {
     function __construct($terminal)
     {
-        parent::__construct($terminal);
-        if (!$this->terminal['HOST']) return false;
-	    
         $this->title    = "Звуковые карты";
         $this->description .= '<b>Работает:</b>&nbsp; только на Виндовс;.<br>';
         $this->description .= '<b>Поддерживаемые возможности:</b>&nbsp;say(), sayTo(), sayReply().';
-        $this->setting      = json_decode($this->terminal['TTS_SETING'], true);
+
+	parent::__construct($terminal);
+        if (!$this->terminal['HOST']) return false;
+	$this->setting      = json_decode($this->terminal['TTS_SETING'], true);
         $this->devicenumber = substr($this->setting['TTS_SOUND_DEVICE'], 0, strpos($this->setting['TTS_SOUND_DEVICE'], '^'));
         $this->devicename   = substr($this->setting['TTS_SOUND_DEVICE'], strpos($this->setting['TTS_SOUND_DEVICE'], '^') + 1);
         register_shutdown_function("catchTimeoutTerminals");
