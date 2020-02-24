b9def3c6164458502745a8003b46545123e9a722
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-24 15:14:00 +0200

    Update mdmpiterminal_tts.addon.php

--- modules/terminals/tts/mdmpiterminal_tts.addon.php
+++ modules/terminals/tts/mdmpiterminal_tts.addon.php
@@ -22,12 +22,7 @@ class mdmpiterminal extends tts_addon
     {
         return $this->sendMajorDroidCommand('tts:' . $message['MESSAGE']);
     }
-    
-    function play($input, $time = 0) //SETTINGS_SITE_LANGUAGE_CODE=код языка
-    {
-        return $this->sendMajorDroidCommand('play:' . $input);
-    }
-    
+
     function ask($phrase, $level = 0)
     {
         return $this->sendMajorDroidCommand('ask:' . $phrase);
@@ -43,38 +38,7 @@ class mdmpiterminal extends tts_addon
         return $this->sendMajorDroidCommand('pause');
     }
     
-    // Get player status
-    function status()
-    {
-        // Defaults
-        $track_id = -1;
-        $length   = 0;
-        $time     = 0;
-        $name     = '';
-        $file     = '';
-        $state    = 'unknown';
-        $volume   = $this->sendMajorDroidCommand('get:volume');
-        $random   = FALSE;
-        $loop     = FALSE;
-        $repeat   = FALSE;
-        
-        $this->data = array(
-                'track_id' => (int) $track_id, //ID of currently playing track (in playlist). Integer. If unknown (playback stopped or playlist is empty) = -1.
-                'name' => (string) $name, //Current speed for playing media. float.
-                'file' => (string) $file, //Current link for media in device. String.
-                'length' => (int) $length, //Track length in seconds. Integer. If unknown = 0. 
-                'time' => (int) $time, //Current playback progress (in seconds). If unknown = 0. 
-                'state' => (string) $state, //Playback status. String: stopped/playing/paused/unknown 
-                'volume' => (int) str_replace("volume:", "", $volume), // Volume level in percent. Integer. Some players may have values greater than 100 answer volume:23.
-                'muted' => (int) false,
-                'random' => (boolean) $random, // Random mode. Boolean. 
-                'loop' => (boolean) $loop, // Loop mode. Boolean.
-                'repeat' => (string) $repeat //Repeat mode. Boolean.
-            );
-        return $this->data;
-    }
-    
-		// Get terminal status
+    // Get terminal status
     function terminal_status()
     {
         // Defaults
@@ -108,7 +72,7 @@ class mdmpiterminal extends tts_addon
             );
     }
 	
-    function sendMajorDroidCommand($cmd)
+    private function sendMajorDroidCommand($cmd)
     {
         if ($this->terminal['HOST']) {
             $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
