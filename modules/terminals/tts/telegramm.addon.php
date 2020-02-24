dbd827ecd9ff82fd805a152434dd6c3b28b228f2
tarasfrompir <tarasfrompir@inbox.ru>
2020-02-24 17:28:43 +0200

    Update telegramm.addon.php

--- modules/terminals/tts/telegramm.addon.php
+++ modules/terminals/tts/telegramm.addon.php
@@ -5,15 +5,16 @@ class telegramm extends tts_addon
     
     function __construct($terminal)
     {
-
+        
         $this->title       = "Telegramm module";
         $this->description = '<b>Описание:</b>&nbsp;Для работы использует &nbsp;<a href="https://mjdm.ru/forum/viewtopic.php?f=5&t=2768&sid=89e1057b5d8345f7983111f006d41154">модуль Телеграм</a>. Без этого модуля ничего работать не будет.<br>';
         $this->description .= '<b>Проверка доступности:</b>&nbsp;service_ping (пингование устройства проводится проверкой состояния сервиса).<br>';
         $this->description .= '<b>Поддерживаемые возможности:</b>&nbsp;say(), sayTo(), sayReply(), ask().';
-
+        
         parent::__construct($terminal);
-        if (!$this->terminal['HOST']) return false;
-	    
+        if (!$this->terminal['HOST'])
+            return false;
+        
         unsubscribeFromEvent('telegram', 'SAY');
         unsubscribeFromEvent('telegram', 'SAYTO');
         unsubscribeFromEvent('telegram', 'ASK');
@@ -67,8 +68,8 @@ class telegramm extends tts_addon
         if (file_exists(DIR_MODULES . 'telegram/telegram.class.php')) {
             include(DIR_MODULES . 'telegram/telegram.class.php');
             $telegram_module = new telegram();
-            $users   = SQLSelect("SELECT * FROM tlg_user ");
-            $c_users = count($users);
+            $users           = SQLSelect("SELECT * FROM tlg_user ");
+            $c_users         = count($users);
             if ($phrase AND $c_users) {
                 for ($j = 0; $j < $c_users; $j++) {
                     $user_id = $users[$j]['USER_ID'];
@@ -92,14 +93,14 @@ class telegramm extends tts_addon
         sleep(1);
         return $this->success;
     }
-
+    
     // ping terminal
     function ping()
     {
         if (file_exists(DIR_MODULES . 'telegram/telegram.class.php')) {
             include(DIR_MODULES . 'telegram/telegram.class.php');
             $telegram_module = new telegram();
-            $result = $telegram_module->getMe();
+            $result          = $telegram_module->getMe();
             if (is_array($result) AND $result["ok"] = true) {
                 $this->success = TRUE;
             } else {
@@ -110,13 +111,13 @@ class telegramm extends tts_addon
         }
         return $this->success;
     }
-	
+    
     // Get terminal status
     function terminal_status()
     {
         // Defaults
         $listening_keyphrase = -1;
-		$volume_media        = -1;
+        $volume_media        = -1;
         $volume_ring         = -1;
         $volume_alarm        = -1;
         $volume_notification = -1;
@@ -126,25 +127,27 @@ class telegramm extends tts_addon
         $brightness          = -1;
         $display_state       = -1;
         $battery             = -1;
-	
+        
         $out_data = array(
-                'listening_keyphrase' =>(string) strtolower($listening_keyphrase), // ключевое слово терминал для  начала распознавания (-1 - не поддерживается терминалом)
-				'volume_media' => (int)$volume_media, // громкость медиа на терминале (-1 - не поддерживается терминалом)
-                'volume_ring' => (int)$volume_ring, // громкость звонка к пользователям на терминале (-1 - не поддерживается терминалом)
-                'volume_alarm' => (int)$volume_alarm, // громкость аварийных сообщений на терминале (-1 - не поддерживается терминалом)
-                'volume_notification' => (int)$volume_notification, // громкость простых сообщений на терминале (-1 - не поддерживается терминалом)
-                'brightness_auto' => (int) $brightness_auto, // автояркость включена или выключена 1 или 0 (-1 - не поддерживается терминалом)
-                'recognition' => (int) $recognition, // распознавание на терминале включена или выключена 1 или 0 (-1 - не поддерживается терминалом)
-                'fullscreen' => (int) $recognition, // полноекранный режим на терминале включена или выключена 1 или 0 (-1 - не поддерживается терминалом)
-				'brightness' => (int) $brightness, // яркость екрана (-1 - не поддерживается терминалом)
-				'battery' => (int) $battery, // заряд акумулятора терминала в процентах (-1 - не поддерживается терминалом)
-                'display_state'=> (int) $display_state, // 1, 0  - состояние дисплея (-1 - не поддерживается терминалом)
-            );
-		
-		// удаляем из массива пустые данные
-		foreach ($out_data as $key => $value) {
-			if ($value == '-1') unset($out_data[$key]); ;
-		}
+            'listening_keyphrase' => (string) strtolower($listening_keyphrase), // ключевое слово терминал для  начала распознавания (-1 - не поддерживается терминалом)
+            'volume_media' => (int) $volume_media, // громкость медиа на терминале (-1 - не поддерживается терминалом)
+            'volume_ring' => (int) $volume_ring, // громкость звонка к пользователям на терминале (-1 - не поддерживается терминалом)
+            'volume_alarm' => (int) $volume_alarm, // громкость аварийных сообщений на терминале (-1 - не поддерживается терминалом)
+            'volume_notification' => (int) $volume_notification, // громкость простых сообщений на терминале (-1 - не поддерживается терминалом)
+            'brightness_auto' => (int) $brightness_auto, // автояркость включена или выключена 1 или 0 (-1 - не поддерживается терминалом)
+            'recognition' => (int) $recognition, // распознавание на терминале включена или выключена 1 или 0 (-1 - не поддерживается терминалом)
+            'fullscreen' => (int) $recognition, // полноекранный режим на терминале включена или выключена 1 или 0 (-1 - не поддерживается терминалом)
+            'brightness' => (int) $brightness, // яркость екрана (-1 - не поддерживается терминалом)
+            'battery' => (int) $battery, // заряд акумулятора терминала в процентах (-1 - не поддерживается терминалом)
+            'display_state' => (int) $display_state // 1, 0  - состояние дисплея (-1 - не поддерживается терминалом)
+        );
+        
+        // удаляем из массива пустые данные
+        foreach ($out_data as $key => $value) {
+            if ($value == '-1')
+                unset($out_data[$key]);
+            ;
+        }
         return $out_data;
     }
 }
