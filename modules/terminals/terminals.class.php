39f835c642c841d6093641503ec41b392afb6d02
tarasfrompir <tarasfrompir@inbox.ru>
2020-03-12 10:28:26 +0200

    Update terminals.class.php

--- modules/terminals/terminals.class.php
+++ modules/terminals/terminals.class.php
@@ -19,8 +19,7 @@ class terminals extends module
      *
      * @access private
      */
-    function __construct()
-    {
+    function __construct() {
         $this->name = "terminals";
         $this->title = "<#LANG_MODULE_TERMINALS#>";
         $this->module_category = "<#LANG_SECTION_SETTINGS#>";
@@ -35,8 +34,7 @@ class terminals extends module
      *
      * @access public
      */
-    function saveParams($data = 1)
-    {
+    function saveParams($data = 1) {
         $data = array();
         if (IsSet($this->id)) {
             $data["id"] = $this->id;
@@ -60,8 +58,7 @@ class terminals extends module
      *
      * @access public
      */
-    function getParams($data = 1)
-    {
+    function getParams($data = 1) {
         global $id;
         global $mode;
         global $view_mode;
@@ -91,8 +88,7 @@ class terminals extends module
      *
      * @access public
      */
-    function run()
-    {
+    function run() {
         global $session;
         $out = array();
         if ($this->action == 'admin') {
@@ -126,31 +122,30 @@ class terminals extends module
      *
      * @access public
      */
-        function admin(&$out)
-    {
+    function admin(&$out) {
         $this->getConfig();
-		$out['LOG_ENABLED'] = $this->config['LOG_ENABLED'];
+        $out['LOG_ENABLED'] = $this->config['LOG_ENABLED'];
 
         if ($this->config['TERMINALS_TIMEOUT']) {
-			$out['TERMINALS_TIMEOUT'] = $this->config['TERMINALS_TIMEOUT'];
-		} else {
-			$out['TERMINALS_TIMEOUT'] = 10;
-		}
+            $out['TERMINALS_TIMEOUT'] = $this->config['TERMINALS_TIMEOUT'];
+        } else {
+            $out['TERMINALS_TIMEOUT'] = 10;
+        }
         if ($this->config['TERMINALS_DINGDONG']) {
-			$out['TERMINALS_DINGDONG'] = $this->config['TERMINALS_DINGDONG'];
-		} else {
-			$out['TERMINALS_DINGDONG'] = 'dingdong.mp3';
-		}
+            $out['TERMINALS_DINGDONG'] = $this->config['TERMINALS_DINGDONG'];
+        } else {
+            $out['TERMINALS_DINGDONG'] = 'dingdong.mp3';
+        }
         if ($this->config['TERMINALS_PING']) {
-			$out['TERMINALS_PING'] = $this->config['TERMINALS_PING'];
-		} else {
-			$out['TERMINALS_PING'] = 27;
-		}
+            $out['TERMINALS_PING'] = $this->config['TERMINALS_PING'];
+        } else {
+            $out['TERMINALS_PING'] = 27;
+        }
         if ($this->config['TERMINALS_CASH_CLEAR']) {
-			$out['TERMINALS_CASH_CLEAR'] = $this->config['TERMINALS_CASH_CLEAR'];
-		} else {
-			$out['TERMINALS_CASH_CLEAR'] = 7;
-		}
+            $out['TERMINALS_CASH_CLEAR'] = $this->config['TERMINALS_CASH_CLEAR'];
+        } else {
+            $out['TERMINALS_CASH_CLEAR'] = 7;
+        }
         if (isset($this->data_source) && !$_GET['data_source'] && !$_POST['data_source']) {
             $out['SET_DATASOURCE'] = 1;
         }
@@ -167,13 +162,13 @@ class terminals extends module
             }
         }
         if ($this->view_mode == 'update_settings') {
-            $this->config['LOG_ENABLED'] = gr('log_enabled');      
-            $this->config['TERMINALS_DINGDONG'] = gr('dingdong_path');      
+            $this->config['LOG_ENABLED'] = gr('log_enabled');
+            $this->config['TERMINALS_DINGDONG'] = gr('dingdong_path');
             $this->config['TERMINALS_TIMEOUT'] = trim(gr('terminals_timeout'));
             $this->config['TERMINALS_PING'] = trim(gr('terminals_ping'));
             $this->config['TERMINALS_CASH_CLEAR'] = trim(gr('terminals_cash_clear'));
             $this->saveConfig();
-            
+
             $this->redirect("?ok=1");
         }
     }
@@ -185,8 +180,7 @@ class terminals extends module
      *
      * @access public
      */
-    function usual(&$out)
-    {
+    function usual(&$out) {
         $this->admin($out);
     }
 
@@ -195,8 +189,7 @@ class terminals extends module
      *
      * @access public
      */
-    function search_terminals(&$out)
-    {
+    function search_terminals(&$out) {
         require(DIR_MODULES . $this->name . '/terminals_search.inc.php');
     }
 
@@ -205,8 +198,7 @@ class terminals extends module
      *
      * @access public
      */
-    function edit_terminals(&$out, $id)
-    {
+    function edit_terminals(&$out, $id) {
         require(DIR_MODULES . $this->name . '/terminals_edit.inc.php');
     }
 
@@ -215,8 +207,7 @@ class terminals extends module
      *
      * @access public
      */
-    function delete_terminals($id)
-    {
+    function delete_terminals($id) {
         if ($rec = getTerminalByID($id)) {
             deleteObject($rec['LINKED_OBJECT']);
             SQLExec('DELETE FROM `terminals` WHERE `ID` = ' . $rec['ID']);
@@ -229,24 +220,23 @@ class terminals extends module
      *
      * @access public
      */
-     function processSubscription($event, $details = '')
-    {
+    function processSubscription($event, $details = '') {
         // если происходит событие SAY_CACHED_READY то запускаемся
-        if ($event == 'SAY_CACHED_READY' ) {
+        if ($event == 'SAY_CACHED_READY') {
             $this->getConfig();
-            if ($this->config['LOG_ENABLED']) DebMes("Processing $event: " . json_encode($details, JSON_UNESCAPED_UNICODE), 'terminals');
+            if ($this->config['LOG_ENABLED']) DebMes("Processing$event: " . json_encode($details, JSON_UNESCAPED_UNICODE), 'terminals');
             // ждем файл сообщения
-            while ($count < 1000 ) {
-		if (file_exists($details['CACHED_FILENAME'])) break;
+            while ($count < 1000) {
+                if (file_exists($details['CACHED_FILENAME'])) break;
                 usleep(10000);
                 $count++;
             }
             if ($this->config['LOG_ENABLED']) DebMes("Wait a file " . $count/100 . " seconds. If >=10 second then PROBLEM NOT GET RIGHT TIME MESSAGE", 'terminals');
-            
 
-	    // берем длинну сообщения
+
+            // берем длинну сообщения
             $count = 0;
-            while ($count<3) {
+            while ($count < 3) {
                 $duration = get_media_info($details['CACHED_FILENAME'])['duration'];
                 if ($duration_norm < $duration) {
                     $duration_norm = $duration;
@@ -260,7 +250,7 @@ class terminals extends module
             } else {
                 $duration = $duration_norm;
             }
-            if ($this->config['LOG_ENABLED']) DebMes("FINISH Processing $event:  - " . $details['CACHED_FILENAME'], 'terminals');
+            if ($this->config['LOG_ENABLED']) DebMes("FINISH Processing$event:  - " . $details['CACHED_FILENAME'], 'terminals');
             $rec['MESSAGE_DURATION'] = $duration;
             $rec['ID'] = $details['ID'];
             $rec['CACHED_FILENAME'] = $details['CACHED_FILENAME'];
@@ -278,46 +268,53 @@ class terminals extends module
                         $tts->ask($details['message'], 9999);
                         if ($this->config['LOG_ENABLED']) DebMes("Sending Message - " . $details['message'] . "to : " . $terminal['NAME'], 'terminals');
                     } else {
-						if ($this->config['LOG_ENABLED']) DebMes("Can not ask  Terminal - " . $terminal['NAME'] , 'terminals');
-					}
+                        if ($this->config['LOG_ENABLED']) DebMes("Can not ask  Terminal - " . $terminal['NAME'] , 'terminals');
+                    }
                 } else {
                     sleep (1);
                     if ($this->config['LOG_ENABLED']) DebMes("Terminal not right configured - " . $terminal['NAME'] , 'terminals');
                 }
-            } catch(Exception $e) {
+            } catch (Exception $e) {
                 if ($this->config['LOG_ENABLED']) DebMes("Terminal terminated, not work addon - " . $terminal['NAME'] , 'terminals');
             }
+        } else if ($event == 'HOURLY') {
+            $this->getConfig();
+            $terminals = SQLSelect("SELECT * FROM `terminals`");
+            foreach ($terminals as $terminal) {
+		sg($terminal['LINKED_OBJECT'] . '.TerminalState', 1);
+		pingTerminalSafe($terminal['NAME'], $terminal);    
+	    }
         } else if ($event == 'DAILY') {
             $this->getConfig();
-			// проверяем, что каталог
-			if (is_dir(DOC_ROOT."/cms/cached/voice/")) {
-				// открываем каталог
-				if ($dh = opendir(DOC_ROOT."/cms/cached/voice/")) {
-				// читаем и выводим все элементы
-				// от первого до последнего
-					while (($file = readdir($dh)) !== false) {
-						// текущее время
-						$time_sec=time();
-						// время изменения файла
-						$time_file=fileatime(DOC_ROOT."/cms/cached/voice/" . $file);
-						// тепрь узнаем сколько прошло времени (в секундах)
-						$time=$time_sec-$time_file;
-						$unlink =DOC_ROOT . "/cms/cached/voice/" . $file;
-						if (is_file($unlink)){
-							if ($time>$this->config['TERMINALS_CASH_CLEAR']*60*60*24){
-								if (unlink($unlink)){
-									echo 'Файл удален';
-								} else {
-									echo 'Ошибка при удалении файла';
-								}
-							}
+            // проверяем, что каталог
+            if (is_dir(DOC_ROOT."/cms/cached/voice/")) {
+                // открываем каталог
+                if ($dh = opendir(DOC_ROOT."/cms/cached/voice/")) {
+                    // читаем и выводим все элементы
+                    // от первого до последнего
+                    while (($file = readdir($dh)) !== false) {
+                        // текущее время
+                        $time_sec = time();
+                        // время изменения файла
+                        $time_file = fileatime(DOC_ROOT."/cms/cached/voice/" . $file);
+                        // тепрь узнаем сколько прошло времени (в секундах)
+                        $time = $time_sec-$time_file;
+                        $unlink = DOC_ROOT . "/cms/cached/voice/" . $file;
+                        if (is_file($unlink)) {
+                            if ($time > $this->config['TERMINALS_CASH_CLEAR']*60*60*24) {
+                                if (unlink($unlink)) {
+                                    echo 'Файл удален';
+                                } else {
+                                    echo 'Ошибка при удалении файла';
+                                }
+                            }
 
-						}
-					}
-					// закрываем каталог
-					closedir($dh);
-				}
-			}
+                        }
+                    }
+                    // закрываем каталог
+                    closedir($dh);
+                }
+            }
 
         }
     }
@@ -329,33 +326,32 @@ class terminals extends module
      *
      * @access private
      */
-    function install($parent_name = "")
-    {
+    function install($parent_name = "") {
         // add class and properties
         addClass('Terminals', 'SDevices');
-	    // name of terminal
+        // name of terminal
         addClassProperty('Terminals', 'name');
-	    // state o terminal busy or not bausy
+        // state o terminal busy or not bausy
         addClassProperty('Terminals', 'TerminalState');
-	    // playerstate file, playlist, volume etc...
+        // playerstate file, playlist, volume etc...
         addClassProperty('Terminals', 'playerdata');
-	    // terminal state brigtnes, screen, battary etc..
+        // terminal state brigtnes, screen, battary etc..
         addClassProperty('Terminals', 'terminaldata');
-	    // linked username
+        // linked username
         addClassProperty('Terminals', 'username');
-	    // controll adress for upnp device
+        // controll adress for upnp device
         addClassProperty('Terminals', 'UPNP_CONTROL_ADDRESS');
-   
+
         // update main terminal
         $terminal = getMainTerminal();
         $terminal['TTS_TYPE'] = 'mainterminal';
         $terminal['CANTTS'] = '1';
         SQLUpdate('terminals', $terminal);
 
-	    // add autorestart cicle
+        // add autorestart cicle
         setGlobal('cycle_terminalsControl','start');
         setGlobal('cycle_terminalsAutoRestart','1');
-	    
+
         // remove old files
         @unlink(DIR_MODULES . 'terminals/tts/majordroid.addon.php');
         @unlink(DIR_MODULES . 'terminals/tts/mediaplayer.addon.php');
@@ -372,24 +368,25 @@ class terminals extends module
         //    }
         //    if (!$terminal['TTS_TYPE'] AND $terminal['PLAYER_TYPE']=='majordroid') {
         //        $terminal['TTS_TYPE']='majordroid_tts';
-        //    }			
+        //    }
         //    if (!$terminal['TTS_TYPE'] AND $terminal['PLAYER_TYPE']=='vlcweb') {
         //        $terminal['TTS_TYPE']='vlcweb_tts';
-        //    }		
+        //    }
         //    $terminal['CANTTS'] = '1';
         //    $terminal['USE_SYSTEM_MML'] = '1';
         //    SQLUpdate('terminals', $terminal);
-        //}        
+        //}
         unsubscribeFromEvent($this->name, 'SAY');
         unsubscribeFromEvent($this->name, 'SAYTO');
         unsubscribeFromEvent($this->name, 'ASK');
         unsubscribeFromEvent($this->name, 'SAYREPLY');
-        
+
         subscribeToEvent($this->name, 'SAY_CACHED_READY', 0);
         subscribeToEvent($this->name, 'ASK', 0);
         subscribeToEvent($this->name, 'DAILY');
-// автодобавления метода который вызывается циклом при ошибках терминала..  
-addClassMethod('Terminals', 'MessageError', '
+        subscribeToEvent($this->name, 'HOURLY');
+        // автодобавления метода который вызывается циклом при ошибках терминала..
+        addClassMethod('Terminals', 'MessageError', '
 // $params["NAME"]; - Имя Терминала
 // $params["MESSAGE"]; - сообщение
 // $params["ERROR"]; - тип ошибки
@@ -397,7 +394,7 @@ addClassMethod('Terminals', 'MessageError', '
 // $params["ORIGINAL_OBJECT_TITLE"]; - привязанный обьект
 // $this->username; - Привязанный Пользователь
 // $this->linkedRoom; - привязанное помещение
-');
+            ');
         parent::install($parent_name);
 
     }
@@ -409,14 +406,14 @@ addClassMethod('Terminals', 'MessageError', '
      *
      * @access public
      */
-    function uninstall()
-    {
+    function uninstall() {
         SQLDropTable('terminals');
         unsubscribeFromEvent($this->name, 'SAY');
         unsubscribeFromEvent($this->name, 'SAYTO');
         unsubscribeFromEvent($this->name, 'ASK');
         unsubscribeFromEvent($this->name, 'SAYREPLY');
         unsubscribeFromEvent($this->name, 'SAY_CACHED_READY');
+        unsubscribeFromEvent($this->name, 'HOURLY');
         unsubscribeFromEvent($this->name, 'DAILY');
         parent::uninstall();
     }
@@ -428,8 +425,7 @@ addClassMethod('Terminals', 'MessageError', '
      *
      * @access private
      */
-    function dbInstall($data)
-    {
+    function dbInstall($data) {
         /*
         terminals - Terminals
         */
@@ -441,8 +437,8 @@ addClassMethod('Terminals', 'MessageError', '
  terminals: CANPLAY int(1) NOT NULL DEFAULT '0'
  terminals: CANTTS int(1) NOT NULL DEFAULT '0'
  terminals: MIN_MSG_LEVEL varchar(255) NOT NULL DEFAULT ''
- terminals: TTS_TYPE char(20) NOT NULL DEFAULT '' 
- terminals: TTS_SETING longtext NOT NULL DEFAULT '' 
+ terminals: TTS_TYPE char(20) NOT NULL DEFAULT ''
+ terminals: TTS_SETING longtext NOT NULL DEFAULT ''
  terminals: PLAYER_TYPE char(20) NOT NULL DEFAULT ''
  terminals: PLAYER_PORT varchar(255) NOT NULL DEFAULT ''
  terminals: PLAYER_USERNAME varchar(255) NOT NULL DEFAULT ''
@@ -454,31 +450,31 @@ addClassMethod('Terminals', 'MessageError', '
  terminals: LATEST_REQUEST_TIME datetime
  terminals: LATEST_ACTIVITY datetime
  terminals: USE_SYSTEM_MML int(1) NOT NULL DEFAULT '1'
- terminals: MESSAGE_VOLUME_LEVEL int(3) NOT NULL DEFAULT '100' 
- terminals: TERMINAL_VOLUME_LEVEL int(3) NOT NULL DEFAULT '100' 
- terminals: LOCATION_ID int(5) NOT NULL DEFAULT '0' 
+ terminals: MESSAGE_VOLUME_LEVEL int(3) NOT NULL DEFAULT '100'
+ terminals: TERMINAL_VOLUME_LEVEL int(3) NOT NULL DEFAULT '100'
+ terminals: LOCATION_ID int(5) NOT NULL DEFAULT '0'
 EOD;
-	    
 
-	    
+
+
         parent::dbInstall($data);
 
-	    
-    //добавляем связанный обьект для всех терминалов необходимо для передачи сообщений
-    $terminals = SQLSelect("SELECT * FROM terminals ");
-    foreach ($terminals as $terminal) {
-        addClassObject('Terminals', 'terminal_'.$terminal['NAME']);
-        $terminal['LINKED_OBJECT'] = 'terminal_'.$terminal['NAME'];
-        $location = gg('terminal_' . $terminal['NAME'] . '.linkedRoom');
-        if ($location_id = SQLSelectOne("SELECT * FROM locations WHERE TITLE = '" . $location . "'")) {
-            $terminal['LOCATION_ID'] = $location_id['ID'];
+
+        //добавляем связанный обьект для всех терминалов необходимо для передачи сообщений
+        $terminals = SQLSelect("SELECT * FROM terminals ");
+        foreach ($terminals as $terminal) {
+            addClassObject('Terminals', 'terminal_'.$terminal['NAME']);
+            $terminal['LINKED_OBJECT'] = 'terminal_'.$terminal['NAME'];
+            $location = gg('terminal_' . $terminal['NAME'] . '.linkedRoom');
+            if ($location_id = SQLSelectOne("SELECT * FROM locations WHERE TITLE = '" . $location . "'")) {
+                $terminal['LOCATION_ID'] = $location_id['ID'];
+            }
+            SQLUpdate('terminals', $terminal);
         }
-        SQLUpdate('terminals', $terminal);
+
+
     }
-	    
-	    
-    }
-// --------------------------------------------------------------------
+    // --------------------------------------------------------------------
 }
 
 /*
