8cd3404fb1621f04e3452ec86d20d01576d3143e
tarasfrompir <tarasfrompir@inbox.ru>
2020-03-28 22:12:24 +0200

    Update addons.php

--- modules/app_player/addons.php
+++ modules/app_player/addons.php
@@ -143,7 +143,7 @@ class app_player_addon {
 	}
 	
     // Play the specified file
-	public function restore_playlist($playlist_id, $playlist_content=array(), $track_id = -1, $time = 0, $state = 'stop') {
+	public function restore_playlist($playlist_id, $playlist_content=array(), $track_id = -1, $time = 0, $state = 'stopped') {
 		/*
 			$input - The path to the file for playback. String.
 			
