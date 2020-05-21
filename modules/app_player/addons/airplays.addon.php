f57658510fdf2b35034ae5d63960b858bf920d64
Sergey Goncharov <34092984+SergMicar@users.noreply.github.com>
2020-05-21 17:14:48 +0300

    update description text

--- modules/app_player/addons/airplays.addon.php
+++ modules/app_player/addons/airplays.addon.php
@@ -12,7 +12,9 @@ class airplays extends app_player_addon
     {
         $this->reset_properties();
         $this->title       = "Airplay";
-        $this->description = '<b>Описание:</b>&nbsp; Воспроизведение звука на всех устройства поддерживающих протокол AirPlay.<br>';
+        $this->description = '<b>Описание:</b>&nbsp; Воспроизведение звука на устройства поддерживающих протокол AirPlay.<br>';
+        $this->description .= '<b>Восстановление воспроизведения после TTS:</b>&nbsp; Нет (если ТТС такого же типа, что и плеер). Если же тип ТТС и тип плеера для терминала различны, то плейлист плеера при ТТС не потеряется при любых обстоятельствах).<br>';
+        $this->description .= '<b>Проверка доступности:</b>&nbsp;ip_ping.<br>';
         $this->description .= '<b>Настройка:</b>&nbsp; Порт доступа по умолчанию 7000 (если по умолчанию, можно не указывать).';
         $this->terminal = $terminal;
         $this->port     = (empty($this->terminal['PLAYER_PORT']) ? 7000 : $this->terminal['PLAYER_PORT']);
