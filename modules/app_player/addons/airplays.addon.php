a12989d6e6f39cc4273ca0f4b33247ced5595e93
Sergey Goncharov <34092984+SergMicar@users.noreply.github.com>
2020-05-21 17:43:46 +0300

    update description text

--- modules/app_player/addons/airplays.addon.php
+++ modules/app_player/addons/airplays.addon.php
@@ -13,6 +13,7 @@ class airplays extends app_player_addon
         $this->reset_properties();
         $this->title       = "Airplay";
         $this->description = '<b>Описание:</b>&nbsp; Воспроизведение звука на устройства поддерживающих протокол AirPlay.<br>';
+        $this->description .= '<b>Восстановление воспроизведения после TTS:</b>&nbsp; Нет (если ТТС такого же типа, что и плеер).<br>';
         $this->description .= '<b>Проверка доступности:</b>&nbsp;ip_ping.<br>';
         $this->description .= '<b>Настройка:</b>&nbsp; Порт доступа по умолчанию 7000 (если по умолчанию, можно не указывать).';
         $this->terminal = $terminal;
