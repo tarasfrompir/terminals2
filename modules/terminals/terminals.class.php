2a0393a7d985eb8a2f7974b9b6dc0a8eb56e380f
tarasfrompir <tarasfrompir@inbox.ru>
2020-05-22 13:53:58 +0300

    Update terminals.class.php

--- modules/terminals/terminals.class.php
+++ modules/terminals/terminals.class.php
@@ -393,11 +393,15 @@ class terminals extends module
                 }
             }
         } else if ($event == 'HOURLY') {
+            // ping player state
+            $terminals = getTerminalsCanPlay();
+            foreach ($terminals as $terminal) {
+                pingTerminalSafe($terminal['NAME'], $terminal , 'CANPLAY');
+            }
             // ping device state
             $terminals = getAllTerminals();
             foreach ($terminals as $terminal) {
                 $out = ping($terminal['HOST']);
-                DebMes($out);
                 if ($out) {
                     $terminal['IS_ONLINE'] = 1;
                 } else {
