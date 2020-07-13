1dbba1822ca8bef7be31d608c0610bc62fa8e484
tarasfrompir <tarasfrompir@inbox.ru>
2020-07-13 17:02:46 +0300

    Update common.class.php

--- lib/common.class.php
+++ lib/common.class.php
@@ -1229,6 +1229,25 @@ function urlsafe_b64encode($string)
     return $data;
 }
 
+function urlsafebase64encode($string)
+{
+    $data = base64_encode($string);
+    $data = str_replace(array('+', '/', '='), array('-', '_', ''), $data);
+    $data = urlencode($data);
+    return $data;
+}
+
+function urlsafebase64decode($string)
+{
+    $data = urldecode($data);
+    $data = str_replace(array('-', '_'), array('+', '/'), $string);
+    $mod4 = strlen($data) % 4;
+    if ($mod4) {
+        $data .= substr('====', $mod4);
+    }
+    return base64_decode($data);
+}
+
 function urlsafe_b64decode($string)
 {
     $data = urldecode($data);
