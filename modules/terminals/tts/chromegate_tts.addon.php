98c7cabf67c843af8224d483dbe2ad0e2ed1788b
tarasfrompir <tarasfrompir@inbox.ru>
2020-09-27 08:13:51 +0300

    Update chromegate_tts.addon.php

--- modules/terminals/tts/chromegate_tts.addon.php
+++ modules/terminals/tts/chromegate_tts.addon.php
@@ -14,7 +14,7 @@ class chromegate_tts extends tts_addon {
     {
         if ($terminal['TITLE']) {
             if ($message['MESSAGE']) {
-	        postToWebSocketQueue($message['EVENT'], array('level' => $message['IMPORTANCE'], 'message' => $message['MESSAGE'], 'destination' => $terminal['TITLE'] ), 'PostEvent');
+	        postToWebSocket($message['EVENT'], array('level' => $message['IMPORTANCE'], 'message' => $message['MESSAGE'], 'destination' => $terminal['TITLE'] ), 'PostEvent');
                 usleep(100000);
 	        $this->success = TRUE;
             } else {
@@ -29,7 +29,7 @@ class chromegate_tts extends tts_addon {
     function ask($phrase, $level=0) //SETTINGS_SITE_LANGUAGE_CODE=код языка
     {
         if ($phrase) {
-            postToWebSocketQueue('ASK', array('level' => $level, 'prompt' => $phrase, 'target' => ''), 'PostEvent');
+            postToWebSocket('ASK', array('level' => $level, 'prompt' => $phrase, 'target' => ''), 'PostEvent');
             sleep(1);
 	    $this->success = TRUE;
         } else {
