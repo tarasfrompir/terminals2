4a2e972adc95c99f5fe62a10a1f9fbbc681a435b
tarasfrompir <tarasfrompir@inbox.ru>
2022-02-03 15:20:22 +0300

    Update yandex_tts.addon.php

--- modules/terminals/tts/yandex_tts.addon.php
+++ modules/terminals/tts/yandex_tts.addon.php
@@ -22,10 +22,10 @@ class yandex_tts extends tts_addon
     public function say_message($message, $terminal) //SETTINGS_SITE_LANGUAGE_CODE=код языка
     {
         if (file_exists(DIR_MODULES . 'yadevices/yadevices.class.php')) {
-            //include_once (DIR_MODULES . 'yadevices/yadevices.class.php');
-            //$yadevice = new yadevices();
+            include_once (DIR_MODULES . 'yadevices/yadevices.class.php');
+            $yadevice = new yadevices();
             $station = SQLSelectOne("SELECT * FROM yastations WHERE IP='".$this->terminal['HOST']."'");
-            if (callAPI('/api/module/yadevices','GET',array('station'=>$station['ID'],'say'=>$message['MESSAGE']))) {
+            if ($yadevice->sendCommandToStation((int)$station['ID'], 'повтори за мной ' . $message['MESSAGE'])) {
                 $this->success = TRUE;
             } else {
                 $this->success = FALSE;
